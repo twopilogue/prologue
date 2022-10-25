133b8aa85776bb899c4106ac783ee36029c16297
t21212 <t21212@naver.com>
2022-10-25 16:56:08 +0900

    [BE] feat: Base64 컨버터 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/controller/PostController.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/controller/PostController.java
@@ -1,4 +1,34 @@
 package com.b208.prologue.api.controller;
 
+import com.b208.prologue.api.response.BaseResponseBody;
+import com.b208.prologue.api.response.PostListResponse;
+import com.b208.prologue.api.service.PostService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/list")
+    @ApiOperation(value = "게시물 목록 조회", notes = "게시물 목록 조회를 위해 Git 통신")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "목록 조회 성공", response = PostListResponse.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+    public ResponseEntity<? extends BaseResponseBody> getPost(@RequestParam String accessToken, @RequestParam String githubId, @RequestParam String repoName, @RequestParam String path){
+
+        List<String> result = postService.getList();
+
+        return ResponseEntity.status(200).body(PostListResponse.of(200, "게시물 목록 조회 성공"));
+    }
 }
---
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/PostService.java
@@ -0,0 +1,8 @@
+package com.b208.prologue.api.service;
+
+import java.util.List;
+
+public interface PostService {
+
+    List<String> getList();
+}
