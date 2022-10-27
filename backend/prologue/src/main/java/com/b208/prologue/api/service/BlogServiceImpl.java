98bd88ce438e3a49d952c53115e279b3bb8e35f6
tjrcks2237 <tjrcks2237@naver.com>
2022-10-27 23:55:28 +0900

    [BE] modify: CreateRepositoryRequest 사용

--- backend/prologue/src/main/java/com/b208/prologue/api/service/BlogServiceImpl.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/BlogServiceImpl.java
@@ -1,9 +1,11 @@
 package com.b208.prologue.api.service;
 
+import com.b208.prologue.api.request.CreateRepositoryRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +16,12 @@ public class BlogServiceImpl implements BlogService {
     @Override
     public void createRepository(String accessToken, String githubId) {
 
-        String dataJson = "{\"name\" : \""+ githubId + ".github.io\"}";
+        CreateRepositoryRequest createRepositoryRequest = new CreateRepositoryRequest(githubId + ".github.io");
 
         webClient.post()
                 .uri("/user/repos")
                 .headers(h -> h.setBearerAuth(accessToken))
-                .bodyValue(dataJson)
+                .body(Mono.just(createRepositoryRequest), CreateRepositoryRequest.class)
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(String.class)
