cf2a561910afaffe8d11e592b68d89f450466891
dustnzlzl <dustn4325@naver.com>
2022-10-28 06:11:57 +0900

    [FE] feat: 공통 태그(Chip)  추가

---
+++ frontend/src/components/Tag.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Chip from "@mui/material/Chip";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import { styled } from "@mui/material";
+
+const randomBrightColor = () => {
+  const randomNum = Math.floor(Math.random() * 5);
+
+  const arrR = [184, 115, 57, 208, 122];
+  const arrG = [138, 119, 61, 173, 150];
+  const arrB = [247, 204, 150, 255, 230];
+  const colorR = arrR[randomNum];
+  const colorG = arrG[randomNum];
+  const colorB = arrB[randomNum];
+
+  return "rgb" + "(" + colorR + "," + colorG + "," + colorB + ")";
+};
+
+const ChipStyled = styled(Chip)(() => ({
+  // backgroundColor: palette.blue_5,
+  backgroundColor: randomBrightColor(),
+  color: "white",
+  "& .MuiChip-deleteIcon": {
+    color: "white",
+    "&:hover": {
+      color: "#c5cae9",
+    },
+  },
+}));
+
+interface Props {
+  label: string;
+  onDelete?: () => void | undefined;
+}
+
+const Tag = ({ label, onDelete }: Props) => {
+  return (
+    <ChipStyled
+      deleteIcon={<HighlightOffIcon />}
+      label={label}
+      onDelete={onDelete}
+    />
+  );
+};
+
+Tag.defaultProps = {
+  label: "chip",
+  onDelete: () => console.log("tag 삭제 함수 넣어주세요"),
+};
+
+export default Tag;
