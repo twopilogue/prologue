9b65dd15b10abdc500f31c4e46ab75981bb84380
김연수 <dustnzlzl@naver.com>
2022-10-27 17:17:58 +0900

    [FE] feat: 공통컴포넌트 추가

--- frontend/src/pages/SamplePage.tsx
+++ frontend/src/pages/SamplePage.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Button from "../components/Button";
-import Check from "@material-ui/icons/Check";
+import Check from "@mui/icons-material/Check";
+import Input from "../components/Input";
+import Text from "../components/Text";
+import SwitchButton from "../components/SwitchButton";
+import { FormControl, FormGroup } from "@mui/material";
 
 function SamplePage() {
+  const [state, setState] = React.useState({
+    logo: false,
+    profile: false,
+    category: false,
+  });
+
   return (
-    <div className="App">
+    <div style={{ textAlign: "center", maxWidth: "500px", margin: "20px" }}>
+      <h3>Text &nbsp;&nbsp;&nbsp;&nbsp;</h3>
+      <Text name="Hello" color="red" />
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <Text />
+      <hr />
       <h3>Button &nbsp;&nbsp;&nbsp;&nbsp;</h3>
       <Button
         label="Button"
@@ -12,9 +27,26 @@ function SamplePage() {
         icon={<Check />}
         onClick={() => console.log("클릭함")}
       />
-      &nbsp;&nbsp;&nbsp;&nbsp;
       <Button />
       <hr />
+      <h3>Input &nbsp;&nbsp;&nbsp;&nbsp;</h3>
+      <Input name="Input" placeholder="Input테스트" />
+      <br />
+      <Input />
+      <hr />
+      <h3>SwitchButton &nbsp;&nbsp;&nbsp;&nbsp;</h3>
+      <FormControl component="fieldset" variant="standard">
+        <FormGroup>
+          <SwitchButton
+            label="블로그 로고"
+            onClick={() => {
+              setState({ ...state, logo: !state.logo });
+            }}
+          />
+          <SwitchButton label="프로필" />
+          <SwitchButton label="카테고리" />
+        </FormGroup>
+      </FormControl>
     </div>
   );
 }
