017a447057948c5431b5b97f6d37325dbc78d2f9
김연수 <dustnzlzl@naver.com>
2022-10-27 13:01:58 +0900

    [FE] feat: 공통컴포넌트 버튼 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import SamplePage from "./pages/SamplePage";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <p>page</p>
-            </>
-          }
-        />
+        <Route path="/" element={<p>Main</p>} />
+        <Route path="/sample" element={<SamplePage />} />
       </Routes>
     </div>
   );
