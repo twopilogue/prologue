19866d6fa8f87250eb486933cc5816b5dd7b4781
yunaghgh <yunaghgh@naver.com>
2022-10-27 10:30:07 +0900

    [BE] modify: RestTemplate에서 WebClient로 변경

--- backend/prologue/src/main/java/com/b208/prologue/api/service/AuthService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/AuthService.java
@@ -1,9 +1,11 @@
 package com.b208.prologue.api.service;
 
+import com.b208.prologue.api.response.github.AuthAccessTokenResponse;
 import com.b208.prologue.api.response.github.AuthUserInfoResponse;
+import reactor.core.publisher.Mono;
 
 public interface AuthService {
     String getUri();
-    String getAccessToken(String code);
-    AuthUserInfoResponse getUserInfo(String accessToken);
+    Mono<AuthAccessTokenResponse> getAccessToken(String code);
+    Mono<AuthUserInfoResponse> getUserInfo(String accessToken);
 }
