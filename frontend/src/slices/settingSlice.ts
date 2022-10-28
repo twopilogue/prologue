deedd987c83aa7d3521d421d32022ff2f3bf69c5
jhkang9820 <jhkang9820@gmail.com>
2022-10-28 09:55:53 +0900

    [FE] feat: 레이아웃 이동 테스트

--- frontend/src/slices/settingSlice.ts
+++ frontend/src/slices/settingSlice.ts
@@ -1,20 +1,13 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { rootState } from "../app/store";
+import { Layouts } from "react-grid-layout";
 
-export interface layoutConfig {
-  i: string;
-  x: number;
-  y: number;
-  w: number;
-  h: number;
-}
-
-export interface layoutListConfig {
-  layoutList: layoutConfig[];
-}
-
-const initialState: layoutListConfig = {
-  layoutList: [],
+const initialState: Layouts = {
+  layoutList: [
+    { i: "a", x: 1, y: 0, w: 1, h: 2 },
+    { i: "b", x: 2, y: 0, w: 1, h: 2 },
+    { i: "c", x: 4, y: 0, w: 1, h: 2 },
+  ],
 };
 
 const settingSlice = createSlice({
