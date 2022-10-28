303f8e287338e4a3ee069d8ea7f29f4347304928
김연수 <dustnzlzl@naver.com>
2022-10-28 17:47:06 +0900

    [FE] feat: Header 추가 구현

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,46 +1,133 @@
 import React from "react";
-import Button from "./Button";
-import styles from "./css/header.module.css";
-
-type User = {
-  name: string;
-};
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import { LightMode, DarkMode } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Typography,
+} from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import styles from "./header.module.css";
+import palette from "./styles/colorPalette";
 
 interface HeaderProps {
-  user?: User;
+  user?: string;
   onLogin?: () => void;
   onLogout?: () => void;
-  onCreateAccount?: () => void;
 }
 
-const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps) => (
-  <header>
-    <div className={styles.wrapper}>
-      <div>
-        <img
-          width="32"
-          height="32"
-          src="https://user-images.githubusercontent.com/83412032/198398260-5da6df5d-7abd-4228-a9e7-471360e16000.png"
-        />
-        <h1> Prologue</h1>
+const GithubButton = styled(Button)(() => ({
+  margin: 3,
+  backgroundColor: "transparent",
+  ".MuiButton-outlined": { color: palette.blue_5 },
+  "&:hover": {
+    backgroundColor: "transparent",
+    borderColor: palette.black,
+    boxShadow: "none",
+  },
+}));
+
+const AvatarStyled = styled(Avatar)(() => ({
+  margin: 3,
+  width: "30px",
+  height: "30px",
+}));
+
+const settings = ["내 정보", "Logout"];
+
+function Header({ user, onLogin }: HeaderProps) {
+  const [backgroudMode, setBackgroudMode] = React.useState(true);
+
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null,
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <header>
+      <div
+        className={styles.wrapper}
+        style={{
+          backgroundColor: backgroudMode ? "white" : "black",
+          color: backgroudMode ? "black" : "white",
+        }}
+      >
+        <div>
+          <img
+            width="32"
+            height="32"
+            src="https://user-images.githubusercontent.com/83412032/198398260-5da6df5d-7abd-4228-a9e7-471360e16000.png"
+          />
+          <h1>Prologue</h1>
+        </div>
+
+        <Stack direction="row" spacing={2} style={{ alignItems: "center" }}>
+          <IconButton sx={{ color: backgroudMode ? "black" : "white" }}>
+            {backgroudMode ? (
+              <LightMode onClick={() => setBackgroudMode(!backgroudMode)} />
+            ) : (
+              <DarkMode onClick={() => setBackgroudMode(!backgroudMode)} />
+            )}
+          </IconButton>
+          {user ? (
+            <>
+              <Box sx={{ flexGrow: 0 }}>
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <AvatarStyled alt="Remy Sharp" src="이미지경로" />
+                </IconButton>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </Box>
+            </>
+          ) : (
+            <>
+              <GithubButton
+                startIcon={<GitHubIcon />}
+                onClick={onLogin}
+                sx={{
+                  color: backgroudMode ? "black" : "white",
+                }}
+              >
+                Login
+              </GithubButton>
+            </>
+          )}
+        </Stack>
       </div>
-      <div>
-        {user ? (
-          <>
-            <span className={styles.welcome}>
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button onClick={onLogout} label="Log out" />
-          </>
-        ) : (
-          <>
-            <Button onClick={onLogin} label="Log in" />
-            <Button onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
-      </div>
-    </div>
-  </header>
-);
+    </header>
+  );
+}
 
 export default Header;
