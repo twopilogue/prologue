09176e83d4de14c7566660092e460db242f11d75
jhkang9820 <jhkang9820@gmail.com>
2022-10-28 13:18:24 +0900

    [FE] feat: 라우터 레이아웃 경로 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import SamplePage from "./pages/SamplePage";
 import Setting from "features/setting/Setting";
+import SettingLayout from "features/setting/SettingLayout";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<p>Main</p>} />
         <Route path="/sample" element={<SamplePage />} />
         <Route path="/setting" element={<Setting />} />
+        <Route path="/layout" element={<SettingLayout />} />
       </Routes>
     </div>
   );
