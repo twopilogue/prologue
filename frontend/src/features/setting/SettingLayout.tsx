8c81e6b50ea8b9df06d9e17e35aa23d9941a0d76
jhkang9820 <jhkang9820@gmail.com>
2022-10-28 13:18:56 +0900

    [FE] feat: 레이아웃 컴포넌트 이동 후 위치 저장 기능

--- frontend/src/features/setting/SettingLayout.tsx
+++ frontend/src/features/setting/SettingLayout.tsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Setting.module.css";
 
+import { Responsive, WidthProvider, Layout } from "react-grid-layout";
+import { useAppSelector } from "app/hooks";
+import { selectLayoutList } from "slices/settingSlice";
+
 const SettingLayout = () => {
+  const ResponsiveGridLayout = WidthProvider(Responsive);
+  const savedLayoutList: Layout[] = useAppSelector(selectLayoutList);
+  const [layoutList, setLayoutList] = useState<Layout[]>(savedLayoutList);
+
+  useEffect(() => {
+    const tmpLayoutList: Layout[] = [];
+    const layoutLength = layoutList.length;
+    for (let i = 0; i < layoutLength; i++) {
+      console.log(layoutList[i]);
+      tmpLayoutList.push({
+        ...layoutList[i],
+        static: true,
+      });
+    }
+    console.log(tmpLayoutList);
+    setLayoutList(tmpLayoutList);
+  }, []);
+
   return (
-    <div>
-      <div className={styles.container}>
-        <div className={styles.box1}>1</div>
-        <div className={styles.box2}>2</div>
-        <div className={styles.box3}>3</div>
+    <ResponsiveGridLayout
+      layouts={{ lg: layoutList }}
+      breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+      cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
+      rowHeight={30}
+      width={1000}
+    >
+      <div className={styles.box1} key="a">
+        Category
       </div>
-      <div className={styles.container}>
-        <div className={styles.box4}>4</div>
-        <div className={styles.box5}>5</div>
-        <div className={styles.box6}>6</div>
+      <div className={styles.box2} key="b">
+        Main
       </div>
-    </div>
+      <div className={styles.box3} key="c">
+        Contents
+      </div>
+    </ResponsiveGridLayout>
   );
 };
 
