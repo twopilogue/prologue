c5db5c5d8ea39b8317a6f224a0b392b6fbd79206
jhkang9820 <jhkang9820@gmail.com>
2022-10-28 15:44:16 +0900

    [FE] feat: 레이아웃 컴포넌트 컬러 관련 패키지 설치

--- frontend/src/features/setting/Setting.tsx
+++ frontend/src/features/setting/Setting.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 // import GridLayout from "react-grid-layout";
 import { Responsive, WidthProvider, Layout, Layouts } from "react-grid-layout";
 import * as ReactDOMServer from "react-dom/server";
@@ -6,19 +6,30 @@ import { selectLayoutList, setLayoutList } from "../../slices/settingSlice";
 import { useAppSelector } from "app/hooks";
 import styles from "./Setting.module.css";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { json, useNavigate } from "react-router-dom";
+import { ChromePicker } from "react-color";
 
 const Setting = () => {
   const ResponsiveGridLayout = WidthProvider(Responsive);
-  const savedLayoutList: Layout[] = useAppSelector(selectLayoutList);
   const dispatch = useDispatch();
   const navigator = useNavigate();
+
+  const savedLayoutList: Layout[] = useAppSelector(selectLayoutList);
+  const [color, setColor] = useState<string>("#ffffff");
+
+  const getLayout = () => {
+    const sessionLayout = sessionStorage.getItem("grid-layout");
+    console.log(JSON.parse(sessionLayout));
+    return sessionLayout
+      ? { lg: JSON.parse(sessionLayout) }
+      : { lg: savedLayoutList };
+  };
 
   const saveLayouts = () => {
     const tmpLayoutList: Layout[] = [];
     const layout = sessionStorage.getItem("grid-layout");
     const layoutJson = JSON.parse(layout);
-
+    console.log(layoutJson);
     const layoutLength = layoutJson.length;
     for (let item = 0; item < layoutLength; item++) {
       const layout: Layout = {
@@ -39,17 +50,25 @@ const Setting = () => {
     sessionStorage.setItem("grid-layout", JSON.stringify(layouts));
   };
 
+  const handleColorChange = (color: any) => {
+    setColor(color);
+  };
+
   return (
     <>
       <ResponsiveGridLayout
-        layouts={{ lg: savedLayoutList }}
+        layouts={getLayout()}
         breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
         cols={{ lg: 5, md: 4, sm: 3, xs: 2, xxs: 1 }}
         rowHeight={30}
         width={1000}
         onLayoutChange={handleLayoutChange}
       >
-        <div className={styles.box1} key="a">
+        <div
+          className={styles.box1}
+          style={{ backgroundColor: `${color}` }}
+          key="a"
+        >
           Blue Eyes Dragon
         </div>
         <div className={styles.box2} key="b">
@@ -60,6 +79,11 @@ const Setting = () => {
         </div>
       </ResponsiveGridLayout>
       <button onClick={saveLayouts}>저장</button>
+
+      <ChromePicker
+        color={color}
+        onChange={(color) => handleColorChange(color.hex)}
+      />
     </>
   );
 };
