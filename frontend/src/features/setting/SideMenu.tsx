72a7fcdcb9a26c4ec0d89bf828d506cca1b2101e
jhkang9820 <jhkang9820@gmail.com>
2022-10-28 17:50:24 +0900

    [FE] feat: 설정 페이지 사이드탭 생성

---
+++ frontend/src/features/setting/SideMenu.tsx
@@ -0,0 +1,54 @@
+import { Tab, Tabs } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { TabPanel } from "./TabMenu";
+
+const SideMenu = () => {
+  const [value, setValue] = React.useState(0);
+
+  const a11yProps = (index: number) => {
+    return {
+      id: `vertical-tab-${index}`,
+      "aria-controls": `vertical-tabpanel-${index}`,
+    };
+  };
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: 224,
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: "divider" }}
+      >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+};
+
+export default SideMenu;
