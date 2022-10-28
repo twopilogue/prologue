df9793407f89c8082fc536669a97591d7557a125
jhkang9820 <jhkang9820@gmail.com>
2022-10-28 17:49:12 +0900

    [FE] fix: 디렉토리 구조 변경

--- frontend/src/features/setting/Setting.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import TabMenu from "./components/TabMenu";
-import LayoutSample from "./layout/LayoutSample";
-
-const Setting = () => {
-  return <TabMenu />;
-};
-
-export default Setting;
---
+++ frontend/src/pages/SettingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import TabMenu from "../features/setting/TabMenu";
+import LayoutSample from "../features/setting/layout/LayoutSample";
+import SideMenu from "features/setting/SideMenu";
+
+const SettingPage = () => {
+  return (
+    <>
+      <SideMenu />
+      <TabMenu />
+    </>
+  );
+};
+
+export default SettingPage;
