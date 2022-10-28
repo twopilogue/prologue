b891a1192e055251a4ea745588cc7331b5624e47
김연수 <dustnzlzl@naver.com>
2022-10-28 17:47:06 +0900

    [FE] feat: Header 추가 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import SamplePage from "./pages/SamplePage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path="/" element={<p>Main</p>} />
         <Route path="/sample" element={<SamplePage />} />
