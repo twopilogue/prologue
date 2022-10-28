75c131226d02ed1fd459b01d0b23b49eefc5dcb2
tjrcks2237 <tjrcks2237@naver.com>
2022-10-28 15:40:27 +0900

    [BE] feat: 레포지토리 조회를 위한 서비스 파일 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/service/BlogService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/BlogService.java
@@ -3,4 +3,5 @@ package com.b208.prologue.api.service;
 public interface BlogService {
     void createRepository(String accessToken, String githubId) throws Exception;
     void deleteRepository(String accessToken, String githubId) throws Exception;
+    boolean checkUserRepository(String accessToken, String githubId) throws Exception;
 }
