1bb0d614b28d79bc6470db82d3942d1054347fc8
dustnzlzl <dustn4325@naver.com>
2022-11-01 01:24:24 +0900

    [FE] test: Text 추가

--- frontend/src/pages/SamplePage.tsx
+++ frontend/src/pages/SamplePage.tsx
@@ -57,11 +57,36 @@ function SamplePage() {
         <SideNavigation />
         <div style={{ display: "flex", flexDirection: "column", flexGrow: 1 }}>
           <div
-            style={{ textAlign: "center", maxWidth: "500px", padding: "20px" }}
+            style={{ textAlign: "center", maxWidth: "700px", padding: "20px" }}
           >
-            <h3>Text &nbsp;&nbsp;&nbsp;&nbsp;</h3>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            <Text />
+            <h3>Text</h3>
+            <Text value="프리텐다드 Regular 1rem 캡션" type="caption" />
+            <Text value="프리텐다드 Regular 1.125rem 본문" type="text" />
+            <Text
+              value="프리텐다드 Regular 1.25rem 영역별 타이틀"
+              type="groupTitle"
+            />
+            <Text
+              value="프리텐다드 SemiBold 1.25rem 영역별 타이틀 강조"
+              type="groupTitleBold"
+            />
+            <Text
+              value="프리텐다드 Regular 1.5rem 본문별 타이틀"
+              type="textTitle"
+            />
+            <Text
+              value="프리텐다드 Regular 2rem 페이지별 타이틀"
+              type="pageTitle"
+            />
+            <Text
+              value="프리텐다드 SemiBold 2rem 페이지별 타이틀 강조"
+              type="pageTitleBold"
+            />
+            <Text value="프리텐다드 Regular 2.5rem 타이틀" type="title" />
+            <Text
+              value="프리텐다드 SemiBold 2.5rem 타이틀 강조"
+              type="titleBold"
+            />
             <hr />
             <h3>Button</h3>
             <Button
@@ -78,7 +103,7 @@ function SamplePage() {
             <br />
             <Input onChange={valueCheck} />
             <hr />
-            <h3>SwitchButton2</h3>
+            <h3>SwitchButton</h3>
             <FormControl component="fieldset" variant="standard">
               <FormGroup>
                 <SwitchButton
