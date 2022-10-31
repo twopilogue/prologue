3248663cfdc394a19089d6c8607da1c850fae475
tjrcks2237 <tjrcks2237@naver.com>
2022-10-31 17:58:11 +0900

    [BE] feat: 블로그 템플릿 적용 api

---
+++ backend/prologue/src/main/java/com/b208/prologue/api/response/github/GetTemplateFileResponse.java
@@ -0,0 +1,21 @@
+package com.b208.prologue.api.response.github;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetTemplateFileResponse {
+
+    @JsonProperty("sha")
+    String path;
+
+    @JsonProperty("sha")
+    String type;
+
+}
