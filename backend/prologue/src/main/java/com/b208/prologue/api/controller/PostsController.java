318683025641cbe52c350c18027384289e13e295
yunaghgh <yunaghgh@naver.com>
2022-10-31 10:59:57 +0900

    [BE] feat: 게시글 수정 api 생성

--- backend/prologue/src/main/java/com/b208/prologue/api/controller/PostsController.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/controller/PostsController.java
@@ -1,5 +1,6 @@
 package com.b208.prologue.api.controller;
 
+import com.b208.prologue.api.request.ModifyDetailPostRequest;
 import com.b208.prologue.api.request.WriteDetailPostRequest;
 import com.b208.prologue.api.response.BaseResponseBody;
 import com.b208.prologue.api.response.DetailPostResponse;
@@ -13,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URLDecoder;
 import java.util.List;
 import java.util.Map;
 
@@ -31,10 +31,10 @@ public class PostsController {
             @ApiResponse(code = 200, message = "목록 조회 성공", response = PostListResponse.class),
             @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
     })
-    public ResponseEntity<? extends BaseResponseBody> getPost(@RequestParam String accessToken, @RequestParam String githubId){
+    public ResponseEntity<? extends BaseResponseBody> getPost(@RequestParam String accessToken, @RequestParam String githubId) {
         Map<String, List<String>> result = postService.getList(accessToken, githubId);
 
-        return ResponseEntity.status(200).body(PostListResponse.of(result,200, "게시물 목록 조회 성공"));
+        return ResponseEntity.status(200).body(PostListResponse.of(result, 200, "게시물 목록 조회 성공"));
     }
 
     @GetMapping("")
@@ -48,8 +48,8 @@ public class PostsController {
 
         GetRepoContentResponse getRepoContentResponse = null;
         try {
-            getRepoContentResponse = postService.getDetailPost(accessToken,githubId,directory);
-            return ResponseEntity.status(200).body(DetailPostResponse.of(getRepoContentResponse,200, "게시글 상세 조회에 성공하였습니다."));
+            getRepoContentResponse = postService.getDetailPost(accessToken, githubId, directory);
+            return ResponseEntity.status(200).body(DetailPostResponse.of(getRepoContentResponse, 200, "게시글 상세 조회에 성공하였습니다."));
         } catch (Exception e) {
             return ResponseEntity.status(400).body(BaseResponseBody.of(400, "게시글 상세 조회에 실패하였습니다."));
         }
@@ -65,11 +65,31 @@ public class PostsController {
     public ResponseEntity<? extends BaseResponseBody> writeDetailPost(@RequestBody WriteDetailPostRequest writeDetailPostRequest) {
 
         try {
-            postService.insertDetailPost(writeDetailPostRequest.getAccessToken(),writeDetailPostRequest.getGithubId(),writeDetailPostRequest.getContent());
+            postService.insertDetailPost(writeDetailPostRequest.getAccessToken(), writeDetailPostRequest.getGithubId(),
+                    writeDetailPostRequest.getContent());
             return ResponseEntity.status(200).body(BaseResponseBody.of(200, "게시글 작성에 성공하였습니다."));
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.status(400).body(BaseResponseBody.of(400, "게시글 작성에 실패하였습니다."));
+        }
+    }
+
+    @PutMapping("")
+    @ApiOperation(value = "GitHub 게시글 수정", notes = "GitHub 블로그 게시글을 수정한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "게시글 수정 성공", response = BaseResponseBody.class),
+            @ApiResponse(code = 400, message = "게시글 수정 실패", response = BaseResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+    public ResponseEntity<? extends BaseResponseBody> modifyDetailPost(@RequestBody ModifyDetailPostRequest modifyDetailPostRequest) {
+
+        try {
+            postService.updateDetailPost(modifyDetailPostRequest.getAccessToken(), modifyDetailPostRequest.getGithubId(),
+                    modifyDetailPostRequest.getDirectory(), modifyDetailPostRequest.getContent(), modifyDetailPostRequest.getSha());
+            return ResponseEntity.status(200).body(BaseResponseBody.of(200, "게시글 수정에 성공하였습니다."));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(400).body(BaseResponseBody.of(400, "게시글 수정에 실패하였습니다."));
         }
     }
 }
