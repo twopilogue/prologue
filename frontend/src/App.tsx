3d048e004076a970e0ce076a7d0ca116e8a5dfe1
jhkang9820 <jhkang9820@gmail.com>
2022-10-31 10:49:07 +0900

    [FE] feat: 파비콘 및 웹사이트 탭 타이틀 (헬멧) 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,23 @@ import "./App.css";
 import SamplePage from "./pages/SamplePage";
 import SettingPage from "pages/SettingPage";
 import SettingLayout from "features/setting/SettingLayout";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<p>Main</p>} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Helmet>
+                <title>Prologue</title>
+              </Helmet>
+              <p>Main</p>
+            </>
+          }
+        />
         <Route path="/sample" element={<SamplePage />} />
         <Route path="/setting" element={<SettingPage />} />
         <Route path="/layout" element={<SettingLayout />} />
