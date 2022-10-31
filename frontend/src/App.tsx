9996935af11568690b531df74e52e523e4492a39
김연수 <dustnzlzl@naver.com>
2022-10-31 17:36:24 +0900

    [FE] feat: router 페이지 경로 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import SamplePage from "./pages/SamplePage";
+import Header from "./components/Header";
+import Landing from "./pages/LandingPage";
+import Create from "./pages/CreatePage";
+import Dashboard from "./pages/DashboardPage";
 import Setting from "features/setting/Setting";
 import SettingLayout from "features/setting/SettingLayout";
 import PostList from "pages/PostListPage";
-import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<p>Main</p>} />
-        <Route path="/sample" element={<SamplePage />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/setting" element={<Setting />} />
         <Route path="/layout" element={<SettingLayout />} />
         <Route path="/post" element={<PostList />} />
+
+        <Route path="/sample" element={<SamplePage />} />
       </Routes>
     </div>
   );
