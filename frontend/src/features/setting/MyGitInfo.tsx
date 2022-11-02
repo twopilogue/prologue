51a74c48d6fbd6ea06965af0b1004447a715fd90
jhkang9820 <jhkang9820@gmail.com>
2022-11-02 14:55:27 +0900

    [FE] feat: 깃허브 프로필 컴포넌트 생성 및 배치

--- frontend/src/features/setting/MyGitInfo.tsx
+++ frontend/src/features/setting/MyGitInfo.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import Text from "components/Text";
+import styles from "./Setting.module.css";
+import ButtonStyled from "components/Button";
 
 const MyGitInfo = () => {
-  return <div></div>;
+  return (
+    <div style={{ margin: "20px" }}>
+      <div className={styles.textPadding} style={{ paddingTop: "0" }}>
+        <Text value="Github 정보" type="groupTitleBold" />
+      </div>
+      <div className={styles.githubContainer}>
+        <div className={styles.githubBox}>
+          <div className={styles.githubProfileImg}></div>
+          <div className={styles.githubProfileTexts}>
+            <div className={styles.githubProfileText}>
+              <Text value="yeonsu-k" type="groupTitleBold" />
+            </div>
+            <div className={styles.githubProfileText}>
+              <Text value="github.com/yeonsu-k" type="caption" />
+            </div>
+          </div>
+          <div className={styles.githubProfileLogout}>
+            <ButtonStyled label="로그아웃" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyGitInfo;
