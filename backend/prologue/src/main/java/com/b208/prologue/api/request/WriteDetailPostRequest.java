b47bc582f5ead16417c1d02f9ac2832fedde54cb
yunaghgh <yunaghgh@naver.com>
2022-11-02 13:43:00 +0900

    [BE] feat: 게시글 작성시 이미지 저장

--- backend/prologue/src/main/java/com/b208/prologue/api/request/WriteDetailPostRequest.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/request/WriteDetailPostRequest.java
@@ -1,23 +1,28 @@
 package com.b208.prologue.api.request;
 
-
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import lombok.Getter;
-import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Getter
-@Service
+@Data
 @ApiModel("WriteDetailPostRequest")
 public class WriteDetailPostRequest {
 
-    @ApiModelProperty(name = "엑세스 토큰", example = "Qdsfhdf...")
+    @ApiModelProperty(name = "엑세스 토큰", example = "Qdsfhdf...", required = true)
     String accessToken;
 
-    @ApiModelProperty(name = "깃허브 아이디", example = "test1234")
+    @ApiModelProperty(name = "깃허브 아이디", example = "test1234", required = true)
     String githubId;
 
-    @ApiModelProperty(name = "게시글 내용", example = "md파일 내용...")
+    @ApiModelProperty(name = "게시글 내용", example = "md파일 내용...", required = true)
     String content;
+
+    @ApiModelProperty(name = "게시글에 넣는 이미지", example = "이미지 파일")
+    List<MultipartFile> files;
 
 }
