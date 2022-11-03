fd89d58dc39335e88b2d3a4e836570345960edc0
dustnzlzl <dustnzlzl@naver.com>
2022-11-04 03:23:33 +0900

    [FE] design: 블로그 생성 및 초기화 페이지 간격 수정

--- frontend/src/features/blog/blogReset.tsx
+++ frontend/src/features/blog/blogReset.tsx
@@ -13,27 +13,25 @@ interface Props {
 
 function BlogReset(value: Props) {
   return (
-    <>
-      <Box className={styles.BlogResetBox}>
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={3}
-        >
-          <Text value={value.title} type="pageTitle" bold />
-          <div className={styles.BlogResetImgDiv}>
-            <img src={value.image} alt={value.title} loading="lazy" />
-          </div>
-          <Stack direction="column" spacing={3} sx={{ py: 1 }}>
-            {value.text.map((value, index) => (
-              <Text key={index} value={value} />
-            ))}
-          </Stack>
-          <Button label="선택" onClick={value.onClick} />
+    <Box className={styles.BlogResetBox}>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+      >
+        <Text value={value.title} type="textTitle" bold />
+        <div className={styles.BlogResetImgDiv}>
+          <img src={value.image} alt={value.title} loading="lazy" />
+        </div>
+        <Stack direction="column" spacing={3} sx={{ py: 1 }}>
+          {value.text.map((value, index) => (
+            <Text key={index} value={value} />
+          ))}
         </Stack>
-      </Box>
-    </>
+        <Button label="선택" onClick={value.onClick} />
+      </Stack>
+    </Box>
   );
 }
 
