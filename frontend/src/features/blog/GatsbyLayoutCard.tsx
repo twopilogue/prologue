09e4422d1788e9c854ca3b329fc1ced92a515b3d
김연수 <dustnzlzl@naver.com>
2022-11-03 18:00:00 +0900

    [FE] feat: 개츠비 테마선택 페이지 추가

---
+++ frontend/src/features/blog/GatsbyLayoutCard.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import { Box, ButtonBase, Typography } from "@mui/material";
+
+interface Props {
+  img: string;
+  title: string;
+  production: string;
+}
+
+function GatsbyLayoutCard() {
+  return (
+    <Box sx={{ backgroundColor: "gray", textAlign: "center" }}>
+      <img src=""></img>
+    </Box>
+  );
+}
+
+export default GatsbyLayoutCard;
