845e2caba4e523c4b7f66ed080860efe07ad4055
김주연 <jyeon3930@naver.com>
2022-11-03 17:46:50 +0900

    [FE] modify: 게시물 관리 페이지 컴포넌트 재생성

---
+++ frontend/src/pages/PostPage.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import styles from "features/post/Post.module.css";
+import Text from "components/Text";
+import ButtonStyled from "components/Button";
+import PostCategoryList from "features/post/PostCategoryList";
+import PostList from "features/post/PostList";
+
+const PostListPage = () => {
+  const [tabButton, setTabButton] = useState("posts");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("선택 값 : ", e.target.value);
+    setTabButton(e.target.value);
+  };
+
+  return (
+    <div className={styles.postPage}>
+      <Text value="게시글 관리" type="groupTitle" bold />
+      <br />
+      <Text
+        value="깃허브 블로그 게시글을 한 눈에 관리가 가능합니다."
+        type="caption"
+        color="gray"
+      />
+
+      <div className={styles.postPageButtons}>
+        <ButtonStyled label="Google Analytics" color="sky" />
+        <ButtonStyled label="게시글 작성" />
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <div className={styles.postPageCategory}>
+          <PostCategoryList />
+        </div>
+        <div className={styles.postPageList}>
+          <PostList />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostListPage;
