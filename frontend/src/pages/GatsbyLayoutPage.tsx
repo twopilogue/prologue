46bd283bf7580dad38da535bc81e24a503ff9e7d
dustnzlzl <dustnzlzl@naver.com>
2022-11-04 03:25:09 +0900

    [FE] feat: 개츠비 테마 선택 컴포넌트 추가 및 배치

--- frontend/src/pages/GatsbyLayoutPage.tsx
+++ frontend/src/pages/GatsbyLayoutPage.tsx
@@ -1,61 +1,19 @@
 import React from "react";
 import { Box, Stack } from "@mui/material";
 import Text from "components/Text";
-import Grid from "@mui/material/Grid";
+import Button from "components/Button"
+
 import GatsbyLayoutCard from "features/blog/GatsbyLayoutCard";
-import resetImg from "assets/blog/blogChoice/RepositoryReset.png";
 
 const LayoutChoicePage = () => {
-  const layouts = [
-    {
-      img: resetImg,
-      title: "gatsby-starter-minimal-blog",
-      Production: "LekoArts",
-    },
-    {
-      img: resetImg,
-      title: "gatsby-starter-foundation",
-      production: "stackrole",
-    },
-    {
-      img: resetImg,
-      title: "Gatsby-Starter-Blog",
-      production: "gatsbyjs",
-    },
-    {
-      img: resetImg,
-      title: "Serial Programmer",
-      production: "sharadcodes",
-    },
-    {
-      img: resetImg,
-      title: "gatsby-netlifycms-starter-template",
-      production: "simarmannsingh",
-    },
-    {
-      img: resetImg,
-      title: "Camera",
-      production: "30%",
-    },
-    {
-      img: resetImg,
-      title: "Breakfast",
-      production: "40%",
-    },
-    {
-      img: resetImg,
-      title: "Burgers",
-      production: "30%",
-    },
-  ];
   return (
-    <Box sx={{ mx: 10 }}>
+    <Box>
       <Stack
         direction="column"
         justifyContent="center"
         alignItems="center"
         spacing={1}
-        sx={{ my: 7.5 }}
+        sx={{ my: "3vw" }}
       >
         <Text value="Gatsby 테마 선택" type="pageTitle" bold />
         <Text
@@ -63,19 +21,14 @@ const LayoutChoicePage = () => {
           type="groupTitle"
         />
       </Stack>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={{ xs: 2, md: 3 }}
-          columns={{ xs: 4, sm: 8, md: 16 }}
-        >
-          {layouts.map((layout, index) => (
-            <Grid item xs={2} sm={4} md={4} key={index}>
-              <GatsbyLayoutCard />
-            </Grid>
-          ))}
-        </Grid>
-      </Box>
+      <Stack
+        direction="column"
+        alignItems="center"
+        spacing={3}
+      >
+        <GatsbyLayoutCard />
+        <Button label="Next" />
+      </Stack>
     </Box>
   );
 };
