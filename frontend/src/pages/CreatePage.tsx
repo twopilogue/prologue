31b7d21a2bc23862bb8106a285825c6a69f4ee31
김연수 <dustnzlzl@naver.com>
2022-11-03 11:09:23 +0900

    [FE] refactor: 블로그 초기화 선택 페이지 리팩토링

--- frontend/src/pages/CreatePage.tsx
+++ frontend/src/pages/CreatePage.tsx
@@ -2,46 +2,39 @@ import React from "react";
 import BlogCreateBox from "features/blog/blogCreate/BlogCreateBox";
 import BlogLayoutSetting from "features/blog/blogCreate/BlogLayoutSetting";
 import BlogStepper from "features/blog/blogCreate/BlogStepper";
-import BlogReset from "features/blog/blogReset/blogReset";
 import Button from "components/Button";
 import { Stack } from "@mui/material";
-import { useParams } from "react-router-dom";
 
 const LandingPage = () => {
   const [BlogCreateClick, setBlogCreateClick] = React.useState(false);
-  const { params } = useParams();
 
   return (
     // 1. 블로그를 생성하지 않은 경우
-    params === "reset" ? (
-      <BlogReset />
-    ) : (
-      <div style={{ padding: "100px" }}>
-        <BlogStepper step={BlogCreateClick ? 1 : 0} />
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            marginTop: "50px",
-          }}
-        >
-          {BlogCreateClick ? (
-            <Stack
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              spacing={2}
-            >
-              <BlogLayoutSetting />
-              <Button label="Next" onClick={() => setBlogCreateClick(false)} />
-            </Stack>
-          ) : (
-            <BlogCreateBox onClick={() => setBlogCreateClick(true)} />
-          )}
-        </div>
+    <div style={{ padding: "100px" }}>
+      <BlogStepper step={BlogCreateClick ? 1 : 0} />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "50px",
+        }}
+      >
+        {BlogCreateClick ? (
+          <Stack
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <BlogLayoutSetting />
+            <Button label="Next" onClick={() => setBlogCreateClick(false)} />
+          </Stack>
+        ) : (
+          <BlogCreateBox onClick={() => setBlogCreateClick(true)} />
+        )}
       </div>
-    )
+    </div>
   );
 };
 
