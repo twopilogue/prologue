8efac27290f543c695fdbe9d2e62bc89b1694c17
jhkang9820 <jhkang9820@gmail.com>
2022-11-03 11:37:30 +0900

    [FE] feat: 카테고리 페이지 기타 설정 컴포넌트 생성 및 배치

---
+++ frontend/src/features/setting/CategoryCntSetting.tsx
@@ -0,0 +1,31 @@
+import Text from "components/Text";
+import React, { useState } from "react";
+import styles from "./Setting.module.css";
+import SwitchButton from "components/SwitchButton";
+
+const CategoryCntSetting = () => {
+  const [checkCnt, setCheckCnt] = useState(false);
+
+  return (
+    <div style={{ margin: "20px" }}>
+      <div
+        className={styles.textPadding}
+        style={{ paddingTop: "0", paddingBottom: "10px" }}
+      >
+        <Text value="기타 설정" type="groupTitle" bold />
+      </div>
+      <div className={styles.gridContainer}>
+        <div style={{ padding: "20px" }}>
+          <SwitchButton
+            label="카테고리별 게시글 수 표시하기"
+            name="checkCnt"
+            checked={checkCnt}
+            onChange={() => setCheckCnt(!checkCnt)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryCntSetting;
