de90d8b1f24f42ad7baed71bbeaa791305489ede
t21212 <t21212@naver.com>
2022-11-04 10:46:37 +0900

    [BE] feat: 블로그 설정 조회 API틀 구현

---
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SettingServiceImpl.java
@@ -0,0 +1,51 @@
+package com.b208.prologue.api.service;
+
+import com.b208.prologue.api.response.github.GetSettingResponse;
+import com.b208.prologue.common.Base64Converter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.io.UnsupportedEncodingException;
+import java.util.StringTokenizer;
+
+@Service
+@RequiredArgsConstructor
+public class SettingServiceImpl implements SettingService {
+
+    private final WebClient webClient;
+    private final Base64Converter base64Converter;
+
+    @Override
+    public Object getBlogSetting(String encodedAccessToken, String githubId) throws Exception {
+        String accessToken = base64Converter.decryptAES256(encodedAccessToken);
+
+        String url = "/repos/" + githubId + "/" + githubId + ".github.io" + "/contents/";
+
+        GetSettingResponse item = webClient.get()
+                .uri(url + "gastby-config.json")
+                .headers(h -> h.setBearerAuth(accessToken))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(GetSettingResponse.class).block();
+
+        StringTokenizer st = new StringTokenizer(item.getContent(), "\n");
+        StringBuilder sb = new StringBuilder();
+
+        int val = st.countTokens();
+
+        for (int i = 0; i < val; i++) {
+            sb.append(st.nextToken());
+        }
+
+        String Line = "";
+        try {
+            Line = base64Converter.decode(sb.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return Line;
+    }
+}
