a030d4a80bc7439270f1b71ea075d0aba7a68cbe
김주연 <jyeon3930@naver.com>
2022-11-04 17:54:14 +0900

    [FE] feat: 게시글 페이지 중첩 라우터 추가

--- frontend/src/pages/PostPage.tsx
+++ frontend/src/pages/PostPage.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import styles from "features/post/Post.module.css";
-import Text from "components/Text";
-import ButtonStyled from "components/Button";
-import PostCategoryList from "features/post/PostCategoryList";
-import PostList from "features/post/PostList";
-import TrendingUpOutlinedIcon from "@mui/icons-material/TrendingUpOutlined";
+import Post from "features/post/Post";
+import { Route, Routes } from "react-router-dom";
+import PostWrite from "features/post/PostWrite";
 
 const PostListPage = () => {
   const [tabButton, setTabButton] = useState("posts");
@@ -16,27 +14,10 @@ const PostListPage = () => {
 
   return (
     <div className={styles.postPage}>
-      <Text value="게시글 관리" type="groupTitle" bold />
-      <br /> <br />
-      <Text
-        value="깃허브 블로그 게시글을 한 눈에 관리가 가능합니다."
-        type="caption"
-        color="gray"
-      />
-      <div className={styles.postPageButtons}>
-        <ButtonStyled
-          label="Google Analytics"
-          color="sky"
-          width="11vw"
-          icon={<TrendingUpOutlinedIcon />}
-        />{" "}
-        &nbsp; &nbsp;
-        <ButtonStyled label="게시글 작성" width="11vw" />
-      </div>
-      <div style={{ display: "flex", marginTop: "1%" }}>
-        <PostCategoryList />
-        <PostList />
-      </div>
+      <Routes>
+        <Route path="/" element={<Post />}></Route>
+        <Route path="/write" element={<PostWrite />}></Route>
+      </Routes>
     </div>
   );
 };
