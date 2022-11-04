e26c65b2e62b8f6081f4a3347803a5e6a8a6653c
김연수 <dustnzlzl@naver.com>
2022-11-04 17:55:23 +0900

    [FE] design: 개설완료시 모달 컴포넌트 분리 & 폭죽 효과 추가

---
+++ frontend/src/features/blog/BlogDashboardMoveModal.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Modal from "components/Modal";
+import { useNavigate } from "react-router-dom";
+import confetti from "canvas-confetti";
+
+function BlogDashboardMoveModal() {
+  const navigate = useNavigate();
+
+  confetti({
+    particleCount: 300,
+    startVelocity: 40,
+    spread: 360,
+    origin: {
+      x: 0.5,
+      // since they fall down, start a bit higher than random
+      y: 0.45,
+    },
+  });
+
+  setTimeout(() => {
+    confetti.reset();
+  }, 3000);
+
+  return (
+    <>
+      <Modal
+        buttonNum={1}
+        oneButtonLabel="대시보드로 이동"
+        oneButtonSet={() => navigate("/dashboard")}
+        text={`🎉 개설이 완료 됐습니다 🎉`}
+      />
+    </>
+  );
+}
+
+export default BlogDashboardMoveModal;
