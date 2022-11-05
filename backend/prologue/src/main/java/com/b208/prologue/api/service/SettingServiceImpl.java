dc6561f31e782a4402bb650e7c2b7a57ae638f54
t21212 <t21212@naver.com>
2022-11-05 23:04:30 +0900

    [BE] modify: 블로그 설정조회 리스폰스 수정에 따른 코드수정

--- backend/prologue/src/main/java/com/b208/prologue/api/service/SettingServiceImpl.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SettingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.b208.prologue.api.service;
 
+import com.b208.prologue.api.response.github.GetRepoContentResponse;
 import com.b208.prologue.api.response.github.GetSettingResponse;
 import com.b208.prologue.common.Base64Converter;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,9 @@ public class SettingServiceImpl implements SettingService {
     private final Base64Converter base64Converter;
 
     @Override
-    public List<Object> getBlogSetting(String encodedAccessToken, String githubId) throws Exception {
+    public List<String> getBlogSetting(String encodedAccessToken, String githubId) throws Exception {
         String accessToken = base64Converter.decryptAES256(encodedAccessToken);
 
-        List<Object> result = new ArrayList<>();
         String url = "/repos/" + githubId + "/" + githubId + ".github.io" + "/contents/";
 
         GetSettingResponse item = webClient.get()
@@ -39,57 +39,42 @@ public class SettingServiceImpl implements SettingService {
             sb.append(st.nextToken());
         }
 
+        List<String> result = new ArrayList<>();
         String Line = "";
         Line = base64Converter.decode(sb.toString());
 
-        String cut = ",\n" + "    },\n" + "  },\n" + "  plugins: \\[";
-        String[] stringVal = Line.split(cut);
+        String[] stringVal = Line.split(",\n" + "  plugins:");
+        String tempVal = stringVal[0].replace("module.exports = {\n", "");
 
-        cut = "siteMetadata: \\{\n" + "    title: `";
-        stringVal = stringVal[0].split(cut);
+        result.add(tempVal);
 
-        cut = "`,\n" + "    author: \\{\n" + "      name: `";
-        stringVal = stringVal[1].split(cut);
-        String blogName = stringVal[0];
-        result.add(blogName);
+        stringVal = tempVal.split("profileImg:");
+        stringVal = stringVal[1].split("description:");
 
-        cut = "`,\n" + "      summary: `";
-        stringVal = stringVal[1].split(cut);
-        String nickName = stringVal[0];
-        result.add(nickName);
+        String imagePath = stringVal[0].replace("\"","");
+        imagePath = imagePath.replace(",","");
+        imagePath = imagePath.replace("}","");
+        imagePath = imagePath.replace(" ","");
+        imagePath = imagePath.replace("\n","");
 
-        cut = "`,\n" + "      profileImg: \"";
-        stringVal = stringVal[1].split(cut);
-        String summary = stringVal[0];
-        result.add(summary);
+        System.out.println(imagePath);
+        System.out.println("zz");
+        result.add(getProfileImage(accessToken, url, imagePath));
 
-        cut = "\",\n" + "    },\n" + "    description: `";
-        stringVal = stringVal[1].split(cut);
-        String profileImgPath = stringVal[0];
-        result.add(profileImgPath);
-
-        cut = "`,\n" + "    siteUrl:";
-        stringVal = stringVal[1].split(cut);
-        String description = stringVal[0];
-        result.add(description);
-
-        Map<String, String> social = new HashMap<>();
-
-        if(stringVal[1].contains("Github") || stringVal[1].contains("Instagram") || stringVal[1].contains("twitter")){
-            cut = "social: \\{\n      ";
-            stringVal = stringVal[1].split(cut);
-            stringVal = stringVal[1].split(",\n      ");
-
-            if(stringVal.length > 0){
-                for (int i = 0; i < stringVal.length; i++){
-                    String[] temp = stringVal[i].split(": ");
-
-                    social.put(temp[0], temp[1].replace("`",""));
-                }
-            }
-        }
-
-        result.add(social);
         return result;
+    }
+
+    public String getProfileImage(String accessToken, String url, String path){
+
+        System.out.println(url+path);
+        GetRepoContentResponse item = webClient.get()
+                .uri(url + path)
+                .headers(h -> h.setBearerAuth(accessToken))
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(GetRepoContentResponse.class).block();
+
+        System.out.println(item.getUrl());
+        return item.getUrl();
     }
 }
