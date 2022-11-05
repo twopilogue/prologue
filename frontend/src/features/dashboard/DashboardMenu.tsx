630d3cca0eaa9700f3fee55337b112f9822dd1c4
dustnzlzl <dustnzlzl@naver.com>
2022-11-06 05:40:12 +0900

    [FE] feat: 대시보드 글작성 및 관리 페이지 이동 컴포넌트 배치

--- frontend/src/features/dashboard/DashboardMenu.tsx
+++ frontend/src/features/dashboard/DashboardMenu.tsx
@@ -1,10 +1,35 @@
 import React from "react";
 import styles from "features/dashboard/Dashboard.module.css";
+import { Container, Stack } from "@mui/system";
+import { Avatar, Link } from "@mui/material";
 
 function DashboardMenu() {
   return (
     <div className={styles.container}>
-      <div>DashboardMenu</div>
+      <Container disableGutters>
+        <Stack spacing={2}>
+          <div className={`${styles.flexColumn} ${styles.menuBox}`}>
+            <Stack justifyContent="center" alignItems="center" spacing={2}>
+              <Avatar sx={{ bgcolor: "white", width: "60px", height: "60px" }}>
+                💬
+              </Avatar>
+              <Link href="/post" underline="none" color="inherit">
+                <div className={styles.menuText}>새 글 작성하기</div>
+              </Link>
+            </Stack>
+          </div>
+          <div className={`${styles.flexColumn} ${styles.menuBox}`}>
+            <Stack justifyContent="center" alignItems="center" spacing={2}>
+              <Avatar sx={{ bgcolor: "white", width: "60px", height: "60px" }}>
+                📄
+              </Avatar>
+              <Link href="/post" underline="none" color="inherit">
+                <div className={styles.menuText}>게시글 관리</div>
+              </Link>
+            </Stack>
+          </div>
+        </Stack>
+      </Container>
     </div>
   );
 }
