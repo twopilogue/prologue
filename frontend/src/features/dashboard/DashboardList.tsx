53387b6888aba84148394157e288ab4867819af5
dustnzlzl <dustnzlzl@naver.com>
2022-11-05 23:01:00 +0900

    [FE] design: 대시보드 페이지 컴포넌트 틀 배치

---
+++ frontend/src/features/dashboard/DashboardList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "features/dashboard/Dashboard.module.css";
+
+function DashboardList() {
+  return (
+    <div className={styles.container}>
+      <div>DashboardList</div>
+    </div>
+  );
+}
+
+export default DashboardList;
---
+++ frontend/src/features/dashboard/DashboardMenu.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "features/dashboard/Dashboard.module.css";
+
+function DashboardMenu() {
+  return (
+    <div className={styles.container}>
+      <div>DashboardMenu</div>
+    </div>
+  );
+}
+
+export default DashboardMenu;
