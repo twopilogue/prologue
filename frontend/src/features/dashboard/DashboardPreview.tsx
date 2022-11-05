ed4a8eb59b4c07a1435956f1ae3a34c394d91495
dustnzlzl <dustnzlzl@naver.com>
2022-11-05 23:01:00 +0900

    [FE] design: 대시보드 페이지 컴포넌트 틀 배치

---
+++ frontend/src/features/dashboard/DashboardPreview.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "features/dashboard/Dashboard.module.css";
+function DashboardPreview() {
+  return (
+    <div className={styles.container}>
+      <div>DashboardPreview</div>
+    </div>
+  );
+}
+
+export default DashboardPreview;
