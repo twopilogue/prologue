5f37f1090cfde73af796ea8e7e68635dc3ef0984
yunaghgh <yunaghgh@naver.com>
2022-11-06 00:52:02 +0900

    [BE] feat: 블로그 페이지 목록 수정 api 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/service/SettingService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SettingService.java
@@ -1,10 +1,13 @@
 package com.b208.prologue.api.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface SettingService {
     Object getBlogSetting(String encodedAccessToken, String githubId) throws Exception;
     String[] getBlogCategory(String encodedAccessToken, String githubId) throws Exception;
     void updateBlogCategory(String encodedAccessToken, String githubId, List<String> category) throws Exception;
     String[] getBlogPages(String encodedAccessToken, String githubId) throws Exception;
+    void updateBlogPages(String encodedAccessToken, String githubId, List<String> pages,
+                         List<Map<String,String>> modifiedPages, List<String> addedPages, List<String> deletedPages) throws Exception;
 }
