8e52e0a54cd4bd42765bf656068ec2d84bf4e696
yunaghgh <yunaghgh@naver.com>
2022-11-05 16:37:44 +0900

    [BE] feat: 카테고리 설정 조회 api 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/service/SettingServiceImpl.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SettingServiceImpl.java
@@ -3,6 +3,9 @@ package com.b208.prologue.api.service;
 import com.b208.prologue.api.response.github.GetSettingResponse;
 import com.b208.prologue.common.Base64Converter;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -15,6 +18,7 @@ public class SettingServiceImpl implements SettingService {
 
     private final WebClient webClient;
     private final Base64Converter base64Converter;
+    private final CommonService commonService;
 
     @Override
     public Object getBlogSetting(String encodedAccessToken, String githubId) throws Exception {
@@ -42,5 +46,26 @@ public class SettingServiceImpl implements SettingService {
         Line = base64Converter.decode(sb.toString());
 
         return Line;
+    }
+
+    @Override
+    public String[] getBlogCategory(String encodedAccessToken, String githubId) throws Exception{
+        String accessToken = base64Converter.decryptAES256(encodedAccessToken);
+
+        String content = commonService.getDetailContent(accessToken, githubId, "customizing-setting.json").getContent();
+        int index = content.indexOf('=');
+        content = content.substring(index+1);
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObj = (JSONObject) jsonParser.parse(content);
+        JSONArray jsonArray = (JSONArray) jsonObj.get("category");
+
+        String[] category = new String[jsonArray.size()-1];
+
+        for(int i=0; i < category.length; i++) {
+            category[i] = jsonArray.get(i+1).toString();
+        }
+
+        return category;
     }
 }
