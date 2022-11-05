d4ac428034642796adb648d6cd13ece21ffffaf5
yunaghgh <yunaghgh@naver.com>
2022-11-05 18:02:48 +0900

    [BE] feat: 카테고리 설정 수정 api 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/service/SettingService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SettingService.java
@@ -1,6 +1,9 @@
 package com.b208.prologue.api.service;
 
+import java.util.List;
+
 public interface SettingService {
     Object getBlogSetting(String encodedAccessToken, String githubId) throws Exception;
     String[] getBlogCategory(String encodedAccessToken, String githubId) throws Exception;
+    void updateBlogCategory(String encodedAccessToken, String githubId, List<String> category) throws Exception;
 }
