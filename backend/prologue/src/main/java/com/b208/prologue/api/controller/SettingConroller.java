de212dddc59cdf8f0ba854444e3fb51795c60885
yunaghgh <yunaghgh@naver.com>
2022-11-05 21:01:47 +0900

    [BE] feat: 블로그 페이지 목록 조회 api 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/controller/SettingConroller.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/controller/SettingConroller.java
@@ -3,6 +3,7 @@ package com.b208.prologue.api.controller;
 import com.b208.prologue.api.request.ModifyBlogCategoryRequest;
 import com.b208.prologue.api.response.BaseResponseBody;
 import com.b208.prologue.api.response.BlogCategoryResponse;
+import com.b208.prologue.api.response.BlogPagesResponse;
 import com.b208.prologue.api.response.GetBlogSettingResponse;
 import com.b208.prologue.api.service.SettingService;
 import io.swagger.annotations.ApiOperation;
@@ -71,6 +72,23 @@ public class SettingConroller {
             return ResponseEntity.status(200).body(BaseResponseBody.of(200, "블로그 카테고리 수정에 성공하였습니다."));
         } catch (Exception e) {
             return ResponseEntity.status(400).body(BaseResponseBody.of(400, "블로그 카테고리 수정에 실패하였습니다."));
+        }
+    }
+
+    @GetMapping("/pages")
+    @ApiOperation(value = "블로그 페이지 목록 조회", notes = "블로그 페이지 목록을 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "블로그 페이지 목록 조회 성공", response = BlogPagesResponse.class),
+            @ApiResponse(code = 400, message = "블로그 페이지 목록 조회 실패", response = BaseResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+    public ResponseEntity<? extends BaseResponseBody> getBlogPages(@RequestParam String accessToken, @RequestParam String githubId) {
+
+        try {
+            String[] pages = settingService.getBlogPages(accessToken, githubId);
+            return ResponseEntity.status(200).body(BlogPagesResponse.of(pages, 200, "블로그 페이지 목록 조회에 성공하였습니다."));
+        } catch (Exception e) {
+            return ResponseEntity.status(400).body(BaseResponseBody.of(400, "블로그 페이지 목록 조회에 실패하였습니다."));
         }
     }
 }
