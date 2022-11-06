4604bb3772b10761489d53bdbf60e48b5fa1c743
t21212 <t21212@naver.com>
2022-11-06 19:54:23 +0900

    [BE] feat: 블로그 설정 변경 API 구현

--- backend/prologue/src/main/java/com/b208/prologue/api/service/SettingService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SettingService.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public interface SettingService {
     List<String> getBlogSetting(String encodedAccessToken, String githubId) throws Exception;
-    void updateBlogSetting(String encodedAccessToken, String githubId, String ninkName, String summary, List<String> techStack, String blogName, String description, List<String> social, MultipartFile imageFile) throws Exception;
+    void updateBlogSetting(String encodedAccessToken, String githubId, String modified) throws Exception;
+    void updateProfileImage(String encodedAccessToken, String githubId, String imgPath, MultipartFile imageFile) throws Exception;
     String[] getBlogCategory(String encodedAccessToken, String githubId) throws Exception;
     void updateBlogCategory(String encodedAccessToken, String githubId, List<String> category) throws Exception;
     String[] getBlogPages(String encodedAccessToken, String githubId) throws Exception;
