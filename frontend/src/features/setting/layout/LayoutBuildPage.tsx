e7f3aeadef8c60230b83a42f06487b6b749760a6
jhkang9820 <jhkang9820@gmail.com>
2022-11-07 17:51:54 +0900

    [FE] feat: 레이아웃 컴포넌트 파일 분리

--- frontend/src/features/setting/layout/LayoutBuildPage.tsx
+++ frontend/src/features/setting/layout/LayoutBuildPage.tsx
@@ -1,21 +1,31 @@
 import { useAppSelector } from "app/hooks";
 import React from "react";
 import { selectCheckList } from "slices/settingSlice";
+import Logo from "./components/Logo";
+import Profile from "./components/Profile";
+import Title from "./components/Title";
 import styles from "./Layout.module.css";
 
 // 1. 위치 전부 세팅
 // 2. checkList 필터 -> 생성할지 말지 정하기
+
+// 3. grid 설정 어떻게 함? -> 기본 모양 만들고, 컴포넌트 얹기.
+// 4. 순서 -> y값 순서대로, 변수에 저장한 컴포넌트를 list에 저장.
 const LayoutBuildPage = () => {
   const checkList = useAppSelector(selectCheckList);
-  console.log(checkList);
+  const logo = <Logo />;
+  const profile = <Profile />;
+  const title = <Title />;
   return (
     <>
-      {checkList.logo ? <div className={styles.logo}>Logo</div> : <></>}
-      {checkList.profile ? <div className={styles.profile}>Profile</div> : <></>}
+      {checkList.logo ? logo : <></>}
+      {checkList.profile ? profile : <></>}
       {checkList.category ? <div className={styles.category}>Category</div> : <></>}
       {checkList.page ? <div className={styles.page}>Page</div> : <></>}
-      <div className={styles.title}>Title</div>
-      <div className={styles.contents}>Contents</div>
+      <div className={styles.display_col}>
+        {title}
+        <div className={styles.contents}>Contents</div>
+      </div>
     </>
   );
 };
