88f018d646449e4c4f8ef22b8c2d99edbbdcb6a3
jhkang9820 <jhkang9820@gmail.com>
2022-11-07 13:14:18 +0900

    [FE] modify: 저장 버튼 위치 수정

--- frontend/src/features/setting/ConfirmButton.tsx
+++ frontend/src/features/setting/ConfirmButton.tsx
@@ -2,14 +2,24 @@ import React from "react";
 import styles from "./Setting.module.css";
 import ButtonStyled from "components/Button";
 import palette from "styles/colorPalette";
+import { useDispatch } from "react-redux";
+import { setCheckList, setComponentLayoutList } from "slices/settingSlice";
 
 interface Props {
   type: string;
+  payload: any;
 }
 
-const ConfirmButton = ({ type }: Props) => {
+const ConfirmButton = ({ type, payload }: Props) => {
+  const dispatch = useDispatch();
   const handleOnClick = () => {
     // if (type === "blogSetting")
+    if (type === "layoutSetting") {
+      console.log("체크", payload.checkList);
+      console.log("컴포넌트", payload.componentLayoutList);
+      dispatch(setCheckList(payload.checkList));
+      dispatch(setComponentLayoutList(payload.componentLayoutList));
+    }
   };
 
   return (
@@ -18,7 +28,7 @@ const ConfirmButton = ({ type }: Props) => {
         <ButtonStyled color="sky" label="취소" />
       </div>
       <div style={{ margin: "10px" }}>
-        <ButtonStyled label="저장" />
+        <ButtonStyled label="저장" onClick={handleOnClick} />
       </div>
     </div>
   );
