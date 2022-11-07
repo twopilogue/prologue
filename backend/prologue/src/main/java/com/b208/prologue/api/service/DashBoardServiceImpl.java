a46335748ec97b90917f51b1f1a0ee08e133aa32
tjrcks2237 <tjrcks2237@naver.com>
2022-11-07 13:31:02 +0900

    [BE] feat: 최근 빌드 시간 조회 api 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/service/DashBoardServiceImpl.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/DashBoardServiceImpl.java
@@ -1,19 +1,14 @@
 package com.b208.prologue.api.service;
 
-import com.b208.prologue.api.response.github.GetRepoContentResponse;
-import com.b208.prologue.api.response.github.GetRepositorySizeResponse;
-import com.b208.prologue.api.response.github.GetTemplateFileResponse;
-import com.b208.prologue.api.response.github.PostGetListResponse;
+import com.b208.prologue.api.response.github.*;
 import com.b208.prologue.common.Base64Converter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -25,7 +20,7 @@ public class DashBoardServiceImpl implements DashBoardService {
     private final CommonService commonService;
 
     @Override
-    public Map<String, Object> getList(String encodedAccessToken, String githubId) throws Exception{
+    public Map<String, Object> getList(String encodedAccessToken, String githubId) throws Exception {
         String accessToken = base64Converter.decryptAES256(encodedAccessToken);
 
         Map<String, Object> result = new HashMap<>();
@@ -34,15 +29,15 @@ public class DashBoardServiceImpl implements DashBoardService {
 
         String url = "/repos/" + githubId + "/" + githubId + ".github.io" + "/contents/";
 
-        PostGetListResponse[] list =  webClient.get()
+        PostGetListResponse[] list = webClient.get()
                 .uri(url + "content/blog")
                 .headers(h -> h.setBearerAuth(accessToken))
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve()
                 .bodyToMono(PostGetListResponse[].class).block();
 
-        for (int i = list.length - 1; i > list.length - 6; i--){
-            if(i < 0) break;
+        for (int i = list.length - 1; i > list.length - 6; i--) {
+            if (i < 0) break;
             content.add(postService.setItem(url, accessToken, list[i].getPath()));
             directory.add(list[i].getName());
         }
@@ -61,18 +56,18 @@ public class DashBoardServiceImpl implements DashBoardService {
         List<Map<String, String>> result = new ArrayList<>();
         Map<String, String> image;
 
-        for (String directory:directories) {
+        for (String directory : directories) {
             GetRepoContentResponse[] responses = commonService.getContentList(accessToken, githubId, "content/blog/" + directory);
             image = new HashMap<>();
             int flag = 0;
 
             for (int i = responses.length - 1; i > responses.length - 6; i--) {
-                if(i < 0) break;
+                if (i < 0) break;
 
                 if (!responses[flag].getName().equals("index.md")) {
                     image.put(directory, responses[flag].getUrl());
                     break;
-                }else{
+                } else {
                     flag++;
                     continue;
                 }
@@ -85,7 +80,7 @@ public class DashBoardServiceImpl implements DashBoardService {
     }
 
     @Override
-    public Double getRepositorySize(String encodedAccessToken, String githubId) throws Exception{
+    public Double getRepositorySize(String encodedAccessToken, String githubId) throws Exception {
         String accessToken = base64Converter.decryptAES256(encodedAccessToken);
 
         GetRepositorySizeResponse getRepositorySizeResponse = webClient.get()
@@ -95,7 +90,21 @@ public class DashBoardServiceImpl implements DashBoardService {
                 .retrieve()
                 .bodyToMono(GetRepositorySizeResponse.class).block();
 
-        return getRepositorySizeResponse.getSize()/100.0;
+        return getRepositorySizeResponse.getSize() / 100.0;
     }
 
+    @Override
+    public String getLatestBuildTime(String encodedAccessToken, String githubId) throws Exception {
+        String accessToken = base64Converter.decryptAES256(encodedAccessToken);
+
+        GetLatestBuildTimeResponse getLatestBuildTimeResponse = webClient.get()
+                .uri("/repos/" + githubId + "/" + githubId + ".github.io/pages/builds/latest")
+                .accept(MediaType.APPLICATION_JSON)
+                .headers(h -> h.setBearerAuth(accessToken))
+                .retrieve()
+                .bodyToMono(GetLatestBuildTimeResponse.class).block();
+
+        SimpleDateFormat dataFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        return dataFormat.format(getLatestBuildTimeResponse.getLastBuildTime());
+    }
 }
