cdcea92dfafcd09d219e82af1b703673bfd49075
tjrcks2237 <tjrcks2237@naver.com>
2022-11-08 16:11:08 +0900

    [BE] fix: 템플릿 적용 시 입력받은 템플릿으로 적용되도록 수정

--- backend/prologue/src/main/java/com/b208/prologue/api/service/BlogService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/BlogService.java
@@ -4,7 +4,7 @@ public interface BlogService {
     void createRepository(String encodedAccessToken, String githubId) throws Exception;
     void deleteRepository(String encodedAccessToken, String githubId) throws Exception;
     boolean checkUserRepository(String encodedAccessToken, String githubId) throws Exception;
-    void selectTemplate(String encodedAccessToken,  String githubId, int templateNumber) throws Exception;
+    void selectTemplate(String encodedAccessToken,  String githubId, String template) throws Exception;
     void createWorkflow(String encodedAccessToken, String githubId) throws Exception;
     void updateDeployBranch(String encodedAccessToken, String githubId) throws Exception;
 }
