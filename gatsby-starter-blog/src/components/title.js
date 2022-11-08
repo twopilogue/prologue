cfc4a5f012fc72ae00c86955c8d2d0aee76457a8
jhkang9820 <jhkang9820@gmail.com>
2022-11-08 09:19:49 +0900

    [FE] feat: 기본 테마 파일 추가

---
+++ gatsby-starter-blog/src/components/title.js
@@ -0,0 +1,31 @@
+import { StaticQuery, Link, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+
+const Title = () => {
+  const height = 300
+  return (
+    <StaticQuery
+      query={graphql`
+        query TitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <StaticImage
+            height={height}
+            src="../images/background-default.jpg"
+            alt="titleImg"
+          />
+        )
+      }}
+    />
+  )
+}
+
+export default Title
