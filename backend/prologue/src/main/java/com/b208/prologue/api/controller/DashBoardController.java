e1a5fdb47b51faba1972f102e4e0fa87719b7d19
t21212 <t21212@naver.com>
2022-11-08 17:02:58 +0900

    [BE] modify: 프론트 요청에  따른 반환내용 변경

--- backend/prologue/src/main/java/com/b208/prologue/api/controller/DashBoardController.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/controller/DashBoardController.java
@@ -1,10 +1,6 @@
 package com.b208.prologue.api.controller;
 
-import com.b208.prologue.api.response.BaseResponseBody;
-import com.b208.prologue.api.response.DateListResponse;
-import com.b208.prologue.api.response.LatestBuildTimeResponse;
-import com.b208.prologue.api.response.PostListResponse;
-import com.b208.prologue.api.response.RepositorySizeResponse;
+import com.b208.prologue.api.response.*;
 import com.b208.prologue.api.service.DashBoardService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -35,10 +31,8 @@ public class DashBoardController {
     })
     public ResponseEntity<? extends BaseResponseBody> getCurrentPost(@RequestParam String accessToken, @RequestParam String githubId) {
         try {
-            Map<String, Object> result = dashBoardService.getList(accessToken, githubId);
-
-            List<Map<String, String>> images = dashBoardService.getListImagese(accessToken, githubId, (List<String>) result.get("directory"));
-            return ResponseEntity.status(200).body(PostListResponse.of(result, images, 200, "게시물 목록 조회 성공"));
+            Map<String, List<String>> result = dashBoardService.getList(accessToken, githubId);
+            return ResponseEntity.status(200).body(DashBoardListResponse.of(result, 200, "게시물 목록 조회 성공"));
         } catch (Exception e) {
             return ResponseEntity.status(400).body(BaseResponseBody.of(400, "게시글 목록 조회에 실패하였습니다."));
         }
--- backend/prologue/src/main/java/com/b208/prologue/api/service/DashBoardService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/DashBoardService.java
@@ -5,8 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 public interface DashBoardService {
-    Map<String, Object> getList(String encodedAccessToken, String githubId) throws Exception;
-    List<Map<String, String>> getListImagese(String encodedAccessToken, String githubId, List<String> directories) throws  Exception;
+    Map<String, List<String>> getList(String encodedAccessToken, String githubId) throws Exception;
     Double getRepositorySize(String encodedAccessToken, String githubId) throws Exception;
     Set<String> getDateList(String encodedAccessToken, String githubId) throws Exception;
     String getLatestBuildTime(String encodedAccessToken, String githubId) throws Exception;
