d71a0d443e92309df72f7900255915c4bec3447d
김연수 <dustnzlzl@naver.com>
2022-11-09 16:49:49 +0900

    [FE] create: 기본테마 선택시 블로그 정보 받는 페이지 추가

---
+++ frontend/src/pages/CustomLayoutPage.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CustomLayoutPage = () => {
+  return (
+    <div style={{ paddingTop: "5%" }}>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "5%",
+        }}
+      >
+        PrologueLayoutPage
+      </div>
+    </div>
+  );
+};
+
+export default CustomLayoutPage;
