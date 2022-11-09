0fe43bfab7f9230afce4e33d30046e2d8fdc31da
t21212 <t21212@naver.com>
2022-11-09 14:49:15 +0900

    [BE] modify: 반환 PostRequest에 담아서 게시글  반환

--- backend/prologue/src/main/java/com/b208/prologue/api/request/PostRequest.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/request/PostRequest.java
@@ -1,2 +1,14 @@
-package com.b208.prologue.api.request;public class PostRequest {
+package com.b208.prologue.api.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostRequest {
+    String title;
+    String date;
+    String content;
+    String directory;
+    String imgUrl;
 }
--- backend/prologue/src/main/java/com/b208/prologue/api/service/PostService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/PostService.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 public interface PostService {
     Map<String, Object> getList(String encodedAccessToken, String githubId, int page) throws Exception;
-    List<Map<String, String>> getListImagese(String encodedAccessToken, String githubId, List<String> directories) throws  Exception;
     void insertDetailPost(String encodedAccessToken, String githubId, String content, List<MultipartFile> files) throws Exception;
     void updateDetailPost(String encodedAccessToken, String githubId, String path, String content, List<MultipartFile> files, List<String> deletedFiles) throws Exception;
     void deleteDetailPost(String encodedAccessToken, String githubId, String directory) throws Exception;
