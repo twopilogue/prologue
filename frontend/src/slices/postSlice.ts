d939abc515f8ad5fcce62843774c2d126fa16576
김주연 <jyeon3930@naver.com>
2022-11-09 17:52:01 +0900

    [FE] feat: postSlice 추가

--- frontend/src/slices/postSlice.ts
+++ frontend/src/slices/postSlice.ts
@@ -1,19 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { rootState } from "app/store";
 
 interface postConfig {
   title: string;
   category: string;
+  tag: [];
+  contents: string;
 }
 
 const initialState: postConfig = {
   title: "",
   category: "",
+  tag: [],
+  contents: "",
 };
 
 const postSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    setPostTitle: (state, { payload }) => {
+      state.title = payload;
+    },
+    setPostCategory: (state, { payload }) => {
+      state.category = payload;
+    },
+    setPostTag: (state, { payload }) => {
+      state.tag = payload;
+    },
+    setPostContents: (state, { payload }) => {
+      state.contents = payload;
+    },
+  },
 });
+export const { setPostTitle, setPostCategory, setPostTag, setPostContents } = postSlice.actions;
+
+export const selectPostTitle = (state: rootState) => state.posts.title;
+export const selectPostCategory = (state: rootState) => state.posts.tag;
+export const selectPostTag = (state: rootState) => state.posts.tag;
+export const selectPostContents = (state: rootState) => state.posts.contents;
 
 export default postSlice.reducer;
