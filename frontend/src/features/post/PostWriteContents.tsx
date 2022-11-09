6c10c3fd9c664af9a5c33538ca91ab4de6dbd08a
김주연 <jyeon3930@naver.com>
2022-11-09 10:51:50 +0900

    [FE] feat: 마크다운 에디터 이미지 hook 기능 수정

--- frontend/src/features/post/PostWriteContents.tsx
+++ frontend/src/features/post/PostWriteContents.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "features/post/PostWrite.module.css";
 import Text from "components/Text";
 import { Editor } from "@toast-ui/react-editor";
 import "@toast-ui/editor/dist/toastui-editor.css";
 
 const PostWriteContents = () => {
+  const [showImages, setShowImages] = useState([]);
+  const [fileList, setFileList] = useState([]);
+
+  const editorRef = useRef<Editor>();
+
   return (
     <div className={styles.postWriteContents}>
       <Text value="내용" type="text" />
       <div style={{ marginTop: "1%" }}></div>
       <Editor
+        ref={editorRef}
         initialValue=""
         previewStyle="vertical"
         height="70vh"
         initialEditType="markdown"
+        hooks={{
+          addImageBlobHook: (blob, callback) => {
+            const uploadFileLists = [...fileList];
+            const imageUrlLists = [...showImages];
+
+            const currentImageUrl = URL.createObjectURL(blob);
+            imageUrlLists.push(currentImageUrl);
+            uploadFileLists.push(blob);
+
+            setShowImages(imageUrlLists);
+            setFileList(uploadFileLists);
+
+            console.log(imageUrlLists);
+            console.log(uploadFileLists);
+            console.log(fileList);
+
+            callback(currentImageUrl);
+            return false;
+          },
+        }}
       ></Editor>
     </div>
   );
