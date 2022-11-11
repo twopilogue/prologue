cc92de1c97f164e97e923ff1157c076ca14e26d0
김주연 <jyeon3930@naver.com>
2022-11-11 16:19:43 +0900

    [FE] feat: 게시글 목록 조회 API 연결

--- frontend/src/features/post/PostListCard.tsx
+++ frontend/src/features/post/PostListCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "features/post/Post.module.css";
 import Text from "components/Text";
 
@@ -8,36 +8,48 @@ interface PostListCardProps {
   tag: [];
   category: string;
   content: string;
+  imageUrl: string;
 }
 
-const PostListCard = ({ title, date, tag, category, content }: PostListCardProps) => {
+const PostListCard = ({ title, date, tag, category, content, imageUrl }: PostListCardProps) => {
+  const [tagList, setTagList] = useState<[]>(tag);
+
   return (
     <div className={styles.postListCard}>
-      <div className={styles.postTitle}>
-        <Text value={title} type="groupTitle" bold />
-      </div>
-      <div className={styles.postDate}>
-        <Text value={date} type="caption" />
-      </div>
-      <br />
+      <div className={styles.postCardTextBox}>
+        <div className={styles.postTitle}>
+          <Text value={title} type="groupTitle" bold />
+        </div>
+        <div className={styles.postDate}>
+          <Text value={date} type="caption" />
+        </div>
+        <br />
 
-      <div className={styles.postTag}>
-        {tag.map((value, key) => (
+        <div className={styles.postTag}>
+          {/* {tagList.map((value, key) => (
           <div key={key}>
             <Text value={`#${value}`} type="caption" /> &nbsp;
           </div>
-        ))}
-      </div>
-      <div className={styles.postCategory}>
-        <Text value={category} type="caption" />
-      </div>
-      <br />
+        ))} */}
+        </div>
+        <div className={styles.postCategory}>
+          <Text value={category} type="caption" />
+        </div>
+        <br />
 
-      <div className={styles.postText}>
-        <Text value={content} type="text" />
+        <div className={styles.postText}>
+          <Text value={content.substring(0, 100)} type="text" />
+        </div>
       </div>
 
-      <div className={styles.postCardImg}></div>
+      <div>
+        <img
+          src={imageUrl}
+          alt=""
+          // onError={(event) => (event.target.style.display = "none")}
+          className={styles.postCardImg}
+        />
+      </div>
     </div>
   );
 };
