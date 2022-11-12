e5ad6a0b31065ce5840ed91a615dbcc066babcab
jhkang9820 <jhkang9820@gmail.com>
2022-11-12 21:06:46 +0900

    [FE] feat: 라디오 버튼 활성화

--- frontend/src/features/setting/detail/components/LogoSetting.tsx
+++ frontend/src/features/setting/detail/components/LogoSetting.tsx
@@ -1,8 +1,13 @@
-import React, { ChangeEvent, Dispatch, MutableRefObject, SetStateAction, useRef } from "react";
+import React, { ChangeEvent, Dispatch, MutableRefObject, SetStateAction, useRef, useState } from "react";
 import Text from "components/Text";
 import RadioButton from "components/RadioButton";
 import styles from "../../Setting.module.css";
 import ButtonStyled from "components/Button";
+import { colorsConfig, selectColors, setColors } from "slices/settingSlice";
+import { useAppSelector } from "app/hooks";
+import { useDispatch } from "react-redux";
+import Input from "components/Input";
+import { RadioGroup } from "@mui/material";
 
 interface LogoSettingProps {
   logoImg: File;
@@ -11,6 +16,17 @@ interface LogoSettingProps {
 
 const LogoSetting = ({ logoImg, setLogoImg }: LogoSettingProps) => {
   const logoImgRef = useRef<HTMLInputElement | null>(null);
+  const colors: colorsConfig = useAppSelector(selectColors);
+  const [radioValue, setRadioValue] = useState("logoText");
+  const dispatch = useDispatch();
+
+  const radioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setRadioValue((event.target as HTMLInputElement).value);
+  };
+
+  const handleLogoText = (e: string) => {
+    dispatch(setColors({ ...colors, logo: { inputText: e } }));
+  };
 
   const controlImgRef = (type: MutableRefObject<HTMLInputElement>) => {
     if (!type.current) return;
@@ -30,7 +46,21 @@ const LogoSetting = ({ logoImg, setLogoImg }: LogoSettingProps) => {
         <Text value="블로그 로고" type="text" bold />
       </div>
       <div className={styles.detailItem}>
-        <RadioButton label="이미지 설정" value="color" checked />
+        <RadioGroup value={radioValue} onChange={radioChange}>
+          <RadioButton label="텍스트 설정" value="logoText" />
+        </RadioGroup>
+      </div>
+      <div style={{ margin: "0 10px 0 10px" }}>
+        <Input
+          placeholder="텍스트 입력"
+          value={colors.logo.inputText}
+          onChange={(e) => handleLogoText(e.target.value)}
+        />
+      </div>
+      <div className={styles.detailItem}>
+        <RadioGroup value={radioValue} onChange={radioChange}>
+          <RadioButton label="이미지 설정" value="logoImg" />
+        </RadioGroup>
       </div>
       <input type="file" style={{ display: "none" }} ref={logoImgRef} onChange={(e) => handleImageUpload(e)} />
       <div className={styles.detailItemImgBtn} onClick={() => controlImgRef(logoImgRef)}>
