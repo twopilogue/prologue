66f943ebb28eedd33c815ab000ffb71a96fbaae5
jhkang9820 <jhkang9820@gmail.com>
2022-11-14 04:40:00 +0900

    [FE] feat: 카테고리 추가/삭제, 카테고리명 수정/저장 기능 구현

---
+++ frontend/src/pages/CategoryPage.tsx
@@ -0,0 +1,14 @@
+import CategoryCntSetting from "features/setting/category/CategoryCntSetting";
+import CategoryLayout from "features/setting/category/CategoryLayout";
+import React from "react";
+
+const CategoryPage = () => {
+  return (
+    <div>
+      <CategoryLayout />
+      <CategoryCntSetting />
+    </div>
+  );
+};
+
+export default CategoryPage;
