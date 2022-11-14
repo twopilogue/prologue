6da3f52965a4a00133c48d602867ae5fd3f4219b
jhkang9820 <jhkang9820@gmail.com>
2022-11-15 04:17:53 +0900

    [FE] feat: 세부 레이아웃 수정 API 연결 테스트

--- frontend/src/features/setting/detail/DetailSelector.tsx
+++ frontend/src/features/setting/detail/DetailSelector.tsx
@@ -1,12 +1,11 @@
-import React, { ChangeEvent, MutableRefObject, useEffect, useState } from "react";
+import React, { ChangeEvent, Dispatch, MutableRefObject, SetStateAction, useEffect, useState } from "react";
 import styles from "../Setting.module.css";
 
 import TitleSetting from "./components/TitleSetting";
 import LogoSetting from "./components/LogoSetting";
 import PageSetting from "./components/PageSetting";
-import CategoryCntSetting from "../category/CategoryCntSetting";
 import { useAppSelector } from "app/hooks";
-import { colorsConfig, selectBlogSettingInfo, selectClickedComp, selectColors, setColors } from "slices/settingSlice";
+import { selectClickedComp } from "slices/settingSlice";
 import { useDispatch } from "react-redux";
 import CategorySetting from "./components/CategorySetting";
 import ProfileSetting from "./components/ProfileSetting";
@@ -17,9 +16,16 @@ export const controlImgRef = (type: MutableRefObject<HTMLInputElement>) => {
   type.current.click();
 };
 
-const DetailSelector = () => {
-  const [titleImg, setTitleImg] = useState(null);
-  const [logoImg, setLogoImg] = useState(null);
+interface Props {
+  logoImg: File;
+  setLogoImg: Dispatch<SetStateAction<File>>;
+  titleImg: File;
+  setTitleImg: Dispatch<SetStateAction<File>>;
+}
+
+const DetailSelector = ({ logoImg, titleImg, setLogoImg, setTitleImg }: Props) => {
+  const dispatch = useDispatch();
+
   const [logoType, setLogoType] = useState("logoText");
   const clicked: string = useAppSelector(selectClickedComp);
   const components: any = {
