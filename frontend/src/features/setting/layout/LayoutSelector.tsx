93b419010bd6921989a67d48128cf2d95547d54f
jhkang9820 <jhkang9820@gmail.com>
2022-11-14 12:50:50 +0900

    [FE] design: 기본 컴포넌트 이미지 생성 및 출력

--- frontend/src/features/setting/layout/LayoutSelector.tsx
+++ frontend/src/features/setting/layout/LayoutSelector.tsx
@@ -1,31 +1,34 @@
 import Text from "components/Text";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Setting.module.css";
 import LayoutSelectorItem from "./../layout/LayoutSelectorItem";
 
+export interface layoutsConfig {
+  idx: number;
+  isClicked: boolean;
+}
+
 const LayoutSelector = () => {
+  const [layoutList, setLayoutList] = useState<layoutsConfig[]>([
+    { idx: 1, isClicked: true },
+    { idx: 2, isClicked: false },
+    { idx: 3, isClicked: false },
+    { idx: 4, isClicked: false },
+    { idx: 5, isClicked: false },
+    { idx: 6, isClicked: false },
+  ]);
   return (
     <>
-      <div
-        className={styles.textPadding}
-        style={{ paddingTop: "0", paddingBottom: "10px" }}
-      >
+      <div className={styles.textPadding} style={{ paddingTop: "0", paddingBottom: "10px" }}>
         <Text value="기본 레이아웃 선택" type="groupTitle" bold />
       </div>
       <div style={{ paddingLeft: "20px" }}>
         <Text value="기본으로 사용할 레이아웃을 선택하세요." type="caption" />
       </div>
       <div className={styles.layoutContainer}>
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
-        <LayoutSelectorItem />
+        {layoutList.map((item: layoutsConfig, i: number) => {
+          return <LayoutSelectorItem key={i} index={i} />;
+        })}
       </div>
     </>
   );
--- frontend/src/features/setting/layout/LayoutSelectorItem.tsx
+++ frontend/src/features/setting/layout/LayoutSelectorItem.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "../Setting.module.css";
+import { layoutsConfig } from "./LayoutSelector";
 
-const LayoutSelectorItem = () => {
-  return <div className={styles.box1}></div>;
+interface Props {
+  index: number;
+  // layoutList: layoutsConfig[];
+  // setLayoutList: React.Dispatch<React.SetStateAction<layoutsConfig[]>>;
+}
+
+const LayoutSelectorItem = ({ index }: Props) => {
+  return (
+    <div className={styles.layoutItem}>
+      <img src={require(`assets/setting/layouts/layout_clicked(${index + 1}).png`)} width={110} height={110} />
+    </div>
+  );
 };
 
 export default LayoutSelectorItem;
