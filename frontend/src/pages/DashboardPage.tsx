025dc4e695b42271aad7270b9122ac5aa0a1c991
dustnzlzl <dustnzlzl@naver.com>
2022-11-16 01:59:22 +0900

    [FE] modify: 대시보드 이동 인증파일 기준으로 변경

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DashboardCalendar from "features/dashboard/DashboardCalendar/DashboardCalendar";
 import DashboardInfo from "features/dashboard/DashboardInfo";
 import DashboardList from "features/dashboard/DashboardList";
 import DashboardMenu from "features/dashboard/DashboardMenu";
 import DashboardPreview from "features/dashboard/DashboardPreview";
 import { Box, Grid, Stack } from "@mui/material";
+import Axios from "api/JsonAxios";
+import api from "api/Api";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { authActions } from "slices/authSlice";
+import { rootState } from "app/store";
 
 const DashboardPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { accessToken, githubId } = useSelector((state: rootState) => state.auth);
+
+  useEffect(() => {
+    getAuthFile();
+  }, []);
+
+  // 서비스 인증 파일 존재 여부
+  async function getAuthFile() {
+    await Axios.get(api.auth.getAuthFile(accessToken, githubId)).then((res) => {
+      if (res.data.checkAuthFile) {
+        dispatch(authActions.authFile({ authFile: true }));
+      } else {
+        dispatch(authActions.authFile({ authFile: false }));
+        navigate("/create/reset");
+      }
+    });
+  }
+
   return (
-    <Box sx={{ mt: 3, minHeight: "88vh"}}>
+    <Box sx={{ mt: 3, minHeight: "88vh" }}>
       <Grid container spacing={2} columns={11}>
         <Grid item xs={3}>
           <DashboardCalendar />
