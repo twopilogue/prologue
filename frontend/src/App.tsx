910c87812a3953b7ec59a96bba2f07d812494b0f
김연수 <dustnzlzl@naver.com>
2022-11-16 17:57:19 +0900

    [FE] feat: 라우터 정리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
-import Landing from "./pages/LandingPage";
-import Create from "./pages/CreatePage";
-import Dashboard from "./pages/DashboardPage";
+import Header from "components/Header";
+import Landing from "pages/LandingPage";
+import Create from "pages/CreatePage";
+import Dashboard from "pages/DashboardPage";
 import SettingPage from "pages/SettingPage";
 import PostRouterPage from "pages/PostRouterPage";
 import BlogReset from "pages/BlogResetPage";
@@ -12,9 +12,15 @@ import GatsbyLayout from "pages/GatsbyLayoutPage";
 import CustomLayoutPage from "pages/CustomLayoutPage";
 import PageRouterPage from "pages/PageRouterPage";
 import LoginOAuthHandler from "features/landing/LoginOAuthHandler";
+import NotFound from "pages/NotFound";
+import AuthRoute from "./AuthRouteTest";
+import { useSelector } from "react-redux";
+import { rootState } from "app/store";
 
 function App() {
   const location = useLocation();
+
+  const { login } = useSelector((state: rootState) => state.auth);
 
   const dashboardStyle = {
     backgroundColor: "#F1F8FF",
@@ -50,10 +56,13 @@ function App() {
           <Route path="/create/reset" element={<BlogReset />} />
           <Route path="/create/custom" element={<CustomLayoutPage />} />
           <Route path="/create/gatsby" element={<GatsbyLayout />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/setting" element={<SettingPage />} />
-          <Route path="/post/*" element={<PostRouterPage />} />
-          <Route path="/page/*" element={<PageRouterPage />} />
+
+          <Route path="/dashboard" element={<AuthRoute authenticated={login} component={<Dashboard />} />} />
+          <Route path="/setting" element={<AuthRoute authenticated={login} component={<SettingPage />} />} />
+          <Route path="/post/*" element={<AuthRoute authenticated={login} component={<PostRouterPage />} />} />
+          <Route path="/page/*" element={<AuthRoute authenticated={login} component={<PageRouterPage />} />} />
+
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </div>
     </div>
