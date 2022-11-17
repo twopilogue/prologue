34e1e10cda64e8341054d2f4e6bf5de0e1af8e1f
jhkang9820 <jhkang9820@gmail.com>
2022-11-17 13:12:58 +0900

    [FE] feat: 컴포넌트 체크 해제 후 위치 설정

--- frontend/src/features/setting/layout/ComponentSelector.tsx
+++ frontend/src/features/setting/layout/ComponentSelector.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../Setting.module.css";
 import SwitchButton from "components/SwitchButton";
 import Text from "components/Text";
 import { useDispatch } from "react-redux";
-import { selectUserCheckList, selectUserComponentLayoutList, setUserCheckList } from "slices/settingSlice";
+import {
+  ComponentCheckConfig,
+  selectComponentLayoutList,
+  selectUserCheckList,
+  selectUserComponentLayoutList,
+  setComponentLayoutList,
+  setUserCheckList,
+  setUserComponentLayoutList,
+} from "slices/settingSlice";
 import { useAppSelector } from "app/hooks";
 import { Layout } from "react-grid-layout";
 
 const ComponentSelector = () => {
   const dispatch = useDispatch();
+  const [unCheckedList, setUncheckedList] = useState<Layout[]>([]);
+  // const componentLayoutList = useAppSelector(selectComponentLayoutList);
+  const userComponentLayoutList = useAppSelector(selectUserComponentLayoutList);
   const checkList = useAppSelector(selectUserCheckList);
+
+  // const [beforeLayout, setBeforeLayout] = useState<Layout[]>([]);
+  // const userCheckList = useAppSelector(selectUserCheckList);
 
   return (
     <div className={styles.checkListContainer}>
@@ -24,6 +38,20 @@ const ComponentSelector = () => {
             checked={checkList.logo}
             onChange={() => {
               dispatch(setUserCheckList({ ...checkList, logo: !checkList.logo }));
+              if (checkList.logo) {
+                userComponentLayoutList.map((it) => {
+                  return it.i === "블로그 로고" ? unCheckedList.push(it) : null;
+                });
+              } else if (!checkList.logo) {
+                const logo = unCheckedList.filter((it) => {
+                  return it.i === "블로그 로고" ? true : false;
+                });
+                const tmpLayoutList: Layout[] = [];
+                tmpLayoutList.push(...userComponentLayoutList, logo[0]);
+                dispatch(setUserComponentLayoutList(tmpLayoutList));
+                setUncheckedList(unCheckedList.filter((it) => it.i !== "블로그 로고"));
+                console.log(unCheckedList);
+              }
             }}
           />
         </div>
@@ -34,6 +62,20 @@ const ComponentSelector = () => {
             checked={checkList.profile}
             onChange={() => {
               dispatch(setUserCheckList({ ...checkList, profile: !checkList.profile }));
+              if (checkList.profile) {
+                userComponentLayoutList.map((it) => {
+                  return it.i === "프로필" ? unCheckedList.push(it) : null;
+                });
+              } else if (!checkList.profile) {
+                const profile = unCheckedList.filter((it) => {
+                  return it.i === "프로필" ? true : false;
+                });
+                const tmpLayoutList: Layout[] = [];
+                tmpLayoutList.push(...userComponentLayoutList, profile[0]);
+                dispatch(setUserComponentLayoutList(tmpLayoutList));
+                setUncheckedList(unCheckedList.filter((it) => it.i !== "프로필"));
+                console.log(unCheckedList);
+              }
             }}
           />
         </div>
@@ -44,6 +86,20 @@ const ComponentSelector = () => {
             checked={checkList.category}
             onChange={() => {
               dispatch(setUserCheckList({ ...checkList, category: !checkList.category }));
+              if (checkList.category) {
+                userComponentLayoutList.map((it) => {
+                  return it.i === "카테고리" ? unCheckedList.push(it) : null;
+                });
+              } else if (!checkList.category) {
+                const category = unCheckedList.filter((it) => {
+                  return it.i === "카테고리" ? true : false;
+                });
+                const tmpLayoutList: Layout[] = [];
+                tmpLayoutList.push(...userComponentLayoutList, category[0]);
+                dispatch(setUserComponentLayoutList(tmpLayoutList));
+                setUncheckedList(unCheckedList.filter((it) => it.i !== "카테고리"));
+                console.log(unCheckedList);
+              }
             }}
           />
         </div>
@@ -52,8 +108,23 @@ const ComponentSelector = () => {
             label="페이지"
             name="page"
             checked={checkList.page}
+            disabled
             onChange={() => {
               dispatch(setUserCheckList({ ...checkList, page: !checkList.page }));
+              if (checkList.page) {
+                userComponentLayoutList.map((it) => {
+                  return it.i === "페이지" ? unCheckedList.push(it) : null;
+                });
+              } else if (!checkList.page) {
+                const page = unCheckedList.filter((it) => {
+                  return it.i === "페이지" ? true : false;
+                });
+                const tmpLayoutList: Layout[] = [];
+                tmpLayoutList.push(...userComponentLayoutList, page[0]);
+                dispatch(setUserComponentLayoutList(tmpLayoutList));
+                setUncheckedList(unCheckedList.filter((it) => it.i !== "페이지"));
+                console.log(unCheckedList);
+              }
             }}
           />
         </div>
