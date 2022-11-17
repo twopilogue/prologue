07cd11c31984cf1a20e846756e57f949056b433c
김주연 <jyeon3930@naver.com>
2022-11-17 11:31:08 +0900

    [FE] fix: Google Analytics 버튼 제거

--- frontend/src/pages/PostManagementPage.tsx
+++ frontend/src/pages/PostManagementPage.tsx
@@ -56,7 +56,7 @@ const PostManagementPage = () => {
       <br /> <br />
       <Text value="깃허브 블로그 게시글을 한 눈에 관리가 가능합니다." type="caption" color="dark_gray" />
       <div className={styles.postPageButtons}>
-        <ButtonStyled label="Google Analytics" color="sky" width="11vw" icon={<TrendingUpOutlinedIcon />} />
+        {/* <ButtonStyled label="Google Analytics" color="sky" width="11vw" icon={<TrendingUpOutlinedIcon />} /> */}
         &nbsp; &nbsp;
         <ButtonStyled
           label="게시글 작성"
