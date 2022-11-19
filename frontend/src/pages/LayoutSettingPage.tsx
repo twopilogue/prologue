f06896099f29850b7e8a3af6681912ef2768ceed
jhkang9820 <jhkang9820@gmail.com>
2022-11-19 18:03:21 +0900

    [FE] modify: 레이아웃 설정 컴포넌트 분리

--- frontend/src/pages/LayoutSettingPage.tsx
+++ frontend/src/pages/LayoutSettingPage.tsx
@@ -5,12 +5,21 @@ import { rootState } from "app/store";
 import ButtonStyled from "components/Button";
 import Text from "components/Text";
 import DefaultLayoutStyles from "features/setting/layout/DefaultLayoutStyles";
+import LayoutContainer from "features/setting/layout/LayoutContainer";
 import LayoutSample from "features/setting/layout/LayoutSample";
 import LayoutSelector from "features/setting/layout/LayoutSelector";
 import React, { useEffect } from "react";
 import { Layout } from "react-grid-layout";
 import { useDispatch, useSelector } from "react-redux";
-import { selectClickedLayoutIdx } from "slices/settingSlice";
+import {
+  ComponentConfig,
+  selectClickedLayoutIdx,
+  setComponentCreated,
+  setOriginComponentLayoutList,
+  setUserCheckList,
+  setUserComponentLayoutList,
+  setUserComponentList,
+} from "slices/settingSlice";
 import styles from "../features/setting/Setting.module.css";
 
 const LayoutSettingPage = () => {
@@ -18,6 +27,32 @@ const LayoutSettingPage = () => {
   const clickedIdx = useAppSelector(selectClickedLayoutIdx);
   const { githubId, accessToken } = useSelector((state: rootState) => state.auth);
   const DefaultLayoutList = DefaultLayoutStyles();
+
+  const getUserLayout = async () => {
+    await Axios.get(api.setting.getLayout(accessToken, githubId))
+      .then((res: any) => {
+        const response = JSON.parse(res.data.layout);
+        console.log("사용자 레이아웃 조회", response);
+        const userComponents: ComponentConfig[] = [];
+        response.layout.map((it: Layout) => {
+          if (it.i === "블로그 로고") userComponents.push({ key: "블로그 로고", id: "logo" });
+          else if (it.i === "프로필") userComponents.push({ key: "프로필", id: "profile" });
+          else if (it.i === "카테고리") userComponents.push({ key: "카테고리", id: "category" });
+          else if (it.i === "페이지") userComponents.push({ key: "페이지", id: "page" });
+          else if (it.i === "타이틀") userComponents.push({ key: "타이틀", id: "title" });
+          else if (it.i === "글 목록") userComponents.push({ key: "글 목록", id: "contents" });
+        });
+        dispatch(setUserComponentLayoutList(response.layout));
+        dispatch(setOriginComponentLayoutList(response.layout));
+        dispatch(setUserComponentList(userComponents));
+        dispatch(setUserCheckList(response.checkList));
+        dispatch(setComponentCreated(true));
+        console.log("-------레이아웃 dispatch 완료-------");
+      })
+      .catch((err: any) => {
+        console.log("실패@", err);
+      });
+  };
 
   const handleOnSave = () => {
     const layoutJson = {
@@ -46,6 +81,10 @@ const LayoutSettingPage = () => {
       });
   };
 
+  useEffect(() => {
+    getUserLayout();
+  }, []);
+
   return (
     <>
       <div className={styles.textPadding} style={{ paddingTop: "0", paddingBottom: "10px" }}>
@@ -55,7 +94,7 @@ const LayoutSettingPage = () => {
         <Text value="기본으로 사용할 레이아웃을 선택하세요." type="caption" />
       </div>
       <LayoutSelector />
-      <LayoutSample />
+      <LayoutContainer />
       <div className={styles.confirmButton}>
         <div style={{ margin: "10px" }}>
           <ButtonStyled color="sky" label="취소" />
