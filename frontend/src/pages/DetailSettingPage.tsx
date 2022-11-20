a1e0690695a0ba88fc48a32374d05fe10444ac04
jhkang9820 <jhkang9820@gmail.com>
2022-11-20 19:36:03 +0900

    [FE] feat: 세부 레이아웃 설정 원상복구 버튼 생성

--- frontend/src/pages/DetailSettingPage.tsx
+++ frontend/src/pages/DetailSettingPage.tsx
@@ -4,6 +4,7 @@ import { rootState } from "app/store";
 import styles from "features/setting/Setting.module.css";
 import Text from "components/Text";
 import ButtonStyled from "components/Button";
+import ReplayIcon from "@mui/icons-material/Replay";
 import { useAppSelector } from "app/hooks";
 import {
   colorsConfig,
@@ -27,6 +28,7 @@ const DetailSettingPage = () => {
   const [saveModalOpen, setSaveModalOpen] = useState<boolean>(false);
   const { githubId, accessToken } = useSelector((state: rootState) => state.auth);
   const colors: colorsConfig = useAppSelector(selectColors);
+  const [originColors, setOriginColors] = useState(null);
 
   const formData = new FormData();
   const dispatch = useDispatch();
@@ -43,40 +45,39 @@ const DetailSettingPage = () => {
 
           console.log("카테고리 변환 결과: ", result.children.category.attributes);
           console.log("프로필 반환 결과: ", result.children.profile.attributes);
-
-          dispatch(
-            setColors({
-              title: {
-                background: result.children.title.attributes["background-color"],
-                text: result.children.titleh3.attributes["color"],
-                // titleHeight:
-                type: res.data.titleColor, // 색이면 true, 이미지면 false
-                titleText: res.data.titleText,
-              },
-              category: {
-                background: result.children.category.attributes["background-color"],
-                text: result.children.categorya.attributes["color"],
-              },
-              page: {
-                background: result.children["page-container"].attributes["background-color"],
-                text: result.children.pagea.attributes["color"],
-                sort: result.children["page-container"].attributes["justify-content"],
-              },
-              profile: {
-                background: result.children.profile.attributes["background-color"],
-                text: result.children.profile.attributes["color"],
-              },
-              contents: {
-                background: result.children["post-list-container"].attributes["background-color"],
-                text: result.children["post-list-container"].attributes["color"],
-              },
-              logo: {
-                background: "#d3d3eb",
-                text: "#ffffff",
-                logoText: res.data.logoText,
-              },
-            }),
-          );
+          const colors = {
+            title: {
+              background: result.children.title.attributes["background-color"],
+              text: result.children.titleh3.attributes["color"],
+              // titleHeight:
+              type: res.data.titleColor, // 색이면 true, 이미지면 false
+              titleText: res.data.titleText,
+            },
+            category: {
+              background: result.children.category.attributes["background-color"],
+              text: result.children.categorya.attributes["color"],
+            },
+            page: {
+              background: result.children["page-container"].attributes["background-color"],
+              text: result.children.pagea.attributes["color"],
+              sort: result.children["page-container"].attributes["justify-content"],
+            },
+            profile: {
+              background: result.children.profile.attributes["background-color"],
+              text: result.children.profile.attributes["color"],
+            },
+            contents: {
+              background: result.children["post-list-container"].attributes["background-color"],
+              text: result.children["post-list-container"].attributes["color"],
+            },
+            logo: {
+              background: "#d3d3eb",
+              text: "#ffffff",
+              logoText: res.data.logoText,
+            },
+          };
+          dispatch(setColors(colors));
+          setOriginColors(colors);
         }
         console.log("DURL?");
       })
@@ -120,6 +121,13 @@ const DetailSettingPage = () => {
     setSaveModalOpen(true);
   };
 
+  const onReset = () => {
+    if (window.confirm("기존 설정으로 돌아갑니다.")) {
+      dispatch(setColors(originColors));
+      dispatch(setClickedComp(initialState.clickedComp));
+    }
+  };
+
   useEffect(() => {
     getDetailSetting();
   }, []);
@@ -134,8 +142,11 @@ const DetailSettingPage = () => {
 
   return (
     <div>
-      <div className={styles.textPadding} style={{ paddingTop: "0", paddingBottom: "10px" }}>
+      <div className={styles.textPadding} style={{ display: "flex", paddingTop: "0", paddingBottom: "10px" }}>
         <Text value="세부 레이아웃 설정" type="groupTitle" bold />
+        <div style={{ marginLeft: "10px", marginTop: "4px" }} onClick={onReset}>
+          <ReplayIcon fontSize="small" />
+        </div>
       </div>
       <div style={{ paddingLeft: "20px" }}>
         <Text value="레이아웃에 원하는 디자인을 선택하여 적용하세요." type="caption" />
