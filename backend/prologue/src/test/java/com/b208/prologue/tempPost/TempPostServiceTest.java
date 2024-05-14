c9bafd93771b9081d125e1f7fe5491a0b94ded8c
yunaghgh <yunaghgh@naver.com>
2024-05-14 19:49:45 +0900

    [BE] test: 서비스 게시글 임시 저장 테스트

--- backend/prologue/src/test/java/com/b208/prologue/tempPost/TempPostServiceTest.java
+++ backend/prologue/src/test/java/com/b208/prologue/tempPost/TempPostServiceTest.java
@@ -2,6 +2,7 @@ package com.b208.prologue.tempPost;
 
 import com.b208.prologue.api.entity.TempPost;
 import com.b208.prologue.api.repository.TempPostRepository;
+import com.b208.prologue.api.request.SaveTempPostRequest;
 import com.b208.prologue.api.service.TempPostServiceImpl;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -28,10 +29,31 @@ class TempPostServiceTest {
     private static final String githubId = "test**";
     private static final Long tempPostId = 0L;
 
+    @Test
+    void 게시글_임시저장() throws Exception {
+        //given
+        final SaveTempPostRequest saveTempPostRequest = SaveTempPostRequest.builder()
+                .githubId(githubId)
+                .title("abc")
+                .build();
+        final TempPost tempPost = TempPost.builder()
+                .tempPostId(0L)
+                .title(saveTempPostRequest.getTitle())
+                .githubId(saveTempPostRequest.getGithubId())
+                .build();
+        doReturn(tempPost).when(tempPostRepository).save(any(TempPost.class));
+
+        //when
+        final Long tempPostId = tempPostService.saveTempPost(saveTempPostRequest);
+
+        //then
+        assertThat(tempPostId).isNotNull().isEqualTo(tempPost.getTempPostId());
+    }
+
     @Nested
     class 임시저장게시글_조회 {
         @Test
-        void 임시저장게시글없음() {
+        void 임시저장게시글없음() throws Exception {
             //given
 
             //when
@@ -42,7 +64,7 @@ class TempPostServiceTest {
         }
 
         @Test
-        void 임시저장게시글있음() {
+        void 임시저장게시글있음() throws Exception {
             //given
             final TempPost tempPost = TempPost.builder()
                     .title("abc")
