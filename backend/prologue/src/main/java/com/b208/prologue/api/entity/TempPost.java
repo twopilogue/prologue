f04e3e043e78f35e55d7bdb3f5b5584bf499ba1d
yunaghgh <yunaghgh@naver.com>
2024-05-14 18:12:21 +0900

    [BE] feat: 임시저장 게시글 관련 엔티티 생성

---
+++ backend/prologue/src/main/java/com/b208/prologue/api/entity/TempPost.java
@@ -0,0 +1,51 @@
+package com.b208.prologue.api.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "temp_post")
+public class TempPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "temp_post_id")
+    private Long tempPostId;
+
+    @Column
+    private String githubId;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private String category;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<String> tags;
+
+    @Column
+    private String content;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createTime;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updateTime;
+}
