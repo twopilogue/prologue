75c0eb30cf7fd285335b523b702e7bf6f68da544
yunaghgh <yunaghgh@naver.com>
2024-05-14 19:52:38 +0900

    [BE] feat: 컨트롤러 게시글 임시 저장 프로덕션 코드 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/controller/TempPostController.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/controller/TempPostController.java
@@ -1,7 +1,9 @@
 package com.b208.prologue.api.controller;
 
+import com.b208.prologue.api.request.SaveTempPostRequest;
 import com.b208.prologue.api.response.BaseResponseBody;
 import com.b208.prologue.api.response.GetTempPostResponse;
+import com.b208.prologue.api.response.SaveTempPostResponse;
 import com.b208.prologue.api.service.TempPostService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -10,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @CrossOrigin("*")
@@ -29,8 +32,31 @@ public class TempPostController {
     })
     public ResponseEntity<? extends BaseResponseBody> getTempPost(@RequestParam String githubId,
                                                                   @RequestParam Long tempPostId) {
-        Map<String, Object> result = tempPostService.getTempPost(githubId, tempPostId);
-        if (result == null) return ResponseEntity.status(400).body(BaseResponseBody.of(400, "임시 저장 게시물 조회에 실패헸습니다."));
-        return ResponseEntity.status(200).body(GetTempPostResponse.of(result, 200, "임시 저장 게시물 조회 성공했습니다."));
+        try {
+            Map<String, Object> result = tempPostService.getTempPost(githubId, tempPostId);
+            if (result == null)
+                return ResponseEntity.status(400).body(BaseResponseBody.of(400, "임시 저장 게시물 조회에 실패헸습니다."));
+            return ResponseEntity.status(200).body(GetTempPostResponse.of(result, 200, "임시 저장 게시물 조회 성공했습니다."));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(400).body(BaseResponseBody.of(400, "임시 저장 게시물 조회에 실패헸습니다."));
+        }
+    }
+
+    @PostMapping("")
+    @ApiOperation(value = "게시글 임시 저장", notes = "게시글을 임시 저장한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "게시글 임시 저장 성공", response = SaveTempPostResponse.class),
+            @ApiResponse(code = 400, message = "게시글 임시 저장 실패", response = BaseResponseBody.class),
+            @ApiResponse(code = 500, message = "서버 오류", response = BaseResponseBody.class)
+    })
+    public ResponseEntity<? extends BaseResponseBody> saveTempPost(@Valid @RequestBody SaveTempPostRequest saveTempPostRequest) {
+        try {
+            Long tempPostId = tempPostService.saveTempPost(saveTempPostRequest);
+            return ResponseEntity.status(200).body(SaveTempPostResponse.of(tempPostId, 200, "게시글 임시 저장에 성공했습니다."));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(400).body(BaseResponseBody.of(400, "게시글 임시 저장에 실패헸습니다."));
+        }
     }
 }
---
+++ backend/prologue/src/main/java/com/b208/prologue/api/request/SaveTempPostRequest.java
@@ -0,0 +1,36 @@
+package com.b208.prologue.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@ApiModel("SaveTempPostRequest")
+public class SaveTempPostRequest {
+
+    @NotBlank
+    @ApiModelProperty(name = "깃허브 아이디", example = "test1234", required = true)
+    String githubId;
+
+    @ApiModelProperty(name = "게시글 제목", example = "게시글 제목", required = true)
+    String title;
+
+    @ApiModelProperty(name = "게시글 설명", example = "게시글 설명", required = true)
+    String description;
+
+    @ApiModelProperty(name = "카테고리", example = "카테고리", required = true)
+    String category;
+
+    @ApiModelProperty(name = "태그 리스트", example = "태그 리스트", required = true)
+    List<String> tags;
+
+    @ApiModelProperty(name = "게시글 내용", example = "게시글 내용", required = true)
+    String content;
+}
