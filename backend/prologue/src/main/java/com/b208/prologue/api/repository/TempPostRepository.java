a68a968ec13a87860a705bd7848a56d624035229
yunaghgh <yunaghgh@naver.com>
2024-05-16 15:30:24 +0900

    [BE] feat: 레포지토리 임시게시글 목록 조회 프로덕션 코드 작성

--- backend/prologue/src/main/java/com/b208/prologue/api/repository/TempPostRepository.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/repository/TempPostRepository.java
@@ -4,9 +4,12 @@ import com.b208.prologue.api.entity.TempPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TempPostRepository extends JpaRepository<TempPost, Long> {
     TempPost findByTempPostIdAndGithubId(final Long tempPostId, final String githubId);
     void deleteByTempPostIdAndGithubId(final Long tempPostId, final String githubId);
     int countByGithubId(final String githubId);
+    List<TempPost> findAllByGithubId(final String githubId);
 }
