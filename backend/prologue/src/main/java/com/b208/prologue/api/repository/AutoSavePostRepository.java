cb1f1b19b9b2a08783ce5cbc42ffca380eb5bfd2
yunaghgh <yunaghgh@naver.com>
2024-05-16 16:57:42 +0900

    [BE] feat: 레포지토리 자동 저장 게시글 조회 프로덕션 코드 작성

---
+++ backend/prologue/src/main/java/com/b208/prologue/api/repository/AutoSavePostRepository.java
@@ -0,0 +1,10 @@
+package com.b208.prologue.api.repository;
+
+import com.b208.prologue.api.entity.AutoSavePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutoSavePostRepository extends JpaRepository<AutoSavePost, String> {
+    AutoSavePost findByGithubId(final String githubId);
+}
