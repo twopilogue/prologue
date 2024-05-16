77ff87fdfe2b7401d49a035457ecef20f59c4996
yunaghgh <yunaghgh@naver.com>
2024-05-16 16:59:00 +0900

    [BE] feat: 서비스 게시글 자동 저장 프로덕션 코드 작성

---
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/AutoSavePostService.java
@@ -0,0 +1,7 @@
+package com.b208.prologue.api.service;
+
+import com.b208.prologue.api.request.AutoSavePostRequest;
+
+public interface AutoSavePostService {
+    void autoSavePost(final AutoSavePostRequest autoSavePostRequest) throws Exception;
+}
