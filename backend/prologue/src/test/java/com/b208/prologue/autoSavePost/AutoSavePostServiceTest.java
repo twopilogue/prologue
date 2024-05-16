e54c6fd456a4ebd2f7ea076667e9bbc060fd5991
yunaghgh <yunaghgh@naver.com>
2024-05-16 18:01:00 +0900

    [BE] test: 자동저장 게시글 시간 조회 서비스 테스트 코드 작성

--- backend/prologue/src/test/java/com/b208/prologue/autoSavePost/AutoSavePostServiceTest.java
+++ backend/prologue/src/test/java/com/b208/prologue/autoSavePost/AutoSavePostServiceTest.java
@@ -1,17 +1,22 @@
 package com.b208.prologue.autoSavePost;
 
 import com.b208.prologue.api.entity.AutoSavePost;
+import com.b208.prologue.api.exception.AutoSavePostException;
 import com.b208.prologue.api.repository.AutoSavePostRepository;
 import com.b208.prologue.api.request.AutoSavePostRequest;
 import com.b208.prologue.api.service.AutoSavePostServiceImpl;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.*;
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AutoSavePostServiceTest {
@@ -55,4 +60,39 @@ class AutoSavePostServiceTest {
         //verify
         verify(autoSavePostRepository, times(1)).existsByGithubId(any(String.class));
     }
+
+    @Nested
+    class 자동저장게시글_시간조회 {
+        @Test
+        void 자동저장게시글_존재안함() throws Exception {
+            //given
+            doReturn(null).when(autoSavePostRepository).findByGithubId(githubId);
+
+            //when
+            final AutoSavePostException result = assertThrows(AutoSavePostException.class, () -> autoSavePostService.getUpdatedTime(githubId));
+
+            //then
+
+            //verify
+            verify(autoSavePostRepository, times(1)).findByGithubId(any(String.class));
+        }
+
+        @Test
+        void 자동저장게시글_존재함() throws Exception {
+            //given
+            final AutoSavePost autoSavePost = AutoSavePost.builder()
+                    .githubId(githubId)
+                    .title("abc")
+                    .updateTime(LocalDateTime.now())
+                    .build();
+            doReturn(autoSavePost).when(autoSavePostRepository).findByGithubId(githubId);
+
+            //when
+            final String updatedAt = autoSavePostService.getUpdatedTime(githubId);
+
+            //then
+            assertThat(updatedAt).isNotNull();
+        }
+    }
+
 }
