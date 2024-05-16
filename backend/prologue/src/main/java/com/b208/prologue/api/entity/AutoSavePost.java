3cee7e18fc7780954427dae801723c0698d9e266
yunaghgh <yunaghgh@naver.com>
2024-05-16 16:51:24 +0900

    [BE] feat: 자동 저장 게시글 엔티티 정의

---
+++ backend/prologue/src/main/java/com/b208/prologue/api/entity/AutoSavePost.java
@@ -0,0 +1,42 @@
+package com.b208.prologue.api.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "auto_save_post")
+public class AutoSavePost {
+
+    @Id
+    @Column(name = "github_id")
+    private String githubId;
+
+    @Column
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    @Column
+    private String category;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    private List<String> tags;
+
+    @Column(length = 10000)
+    private String content;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updateTime;
+}
