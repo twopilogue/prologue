bb8e895473ae36c87c47d3f127e273500d7bc040
yunaghgh <yunaghgh@naver.com>
2024-05-17 18:11:28 +0900

    [BE] feat: 레포지토리 업데이트 날짜 기준으로 기간이 지난 자동 저장글 삭제

--- backend/prologue/src/main/java/com/b208/prologue/api/repository/AutoSavePostRepository.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/repository/AutoSavePostRepository.java
@@ -2,11 +2,19 @@ package com.b208.prologue.api.repository;
 
 import com.b208.prologue.api.entity.AutoSavePost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface AutoSavePostRepository extends JpaRepository<AutoSavePost, String> {
     AutoSavePost findByGithubId(final String githubId);
     boolean existsByGithubId(final String githubId);
     void deleteByGithubId(final String githubId);
+
+    @Modifying
+    @Query("delete from AutoSavePost a where a.updateTime < :target")
+    void deleteByUpdateTimeBefore(final LocalDateTime target);
 }
