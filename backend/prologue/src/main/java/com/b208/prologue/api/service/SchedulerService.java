1e6011d1f23ac983b145c57919b43f3c969f7490
yunaghgh <yunaghgh@naver.com>
2024-05-17 18:14:41 +0900

    [BE] feat: 매일 설정한 시간마다 기간이 지난 자동 저장글 삭제

--- backend/prologue/src/main/java/com/b208/prologue/api/service/SchedulerService.java
+++ backend/prologue/src/main/java/com/b208/prologue/api/service/SchedulerService.java
@@ -10,10 +10,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class SchedulerService {
 
     private final TempPostService tempPostService;
+    private final AutoSavePostService autoSavePostService;
 
     @Transactional
     @Scheduled(cron = "0 30 3 * * *", zone = "Asia/Seoul")
     public void deleteInvalidDateTempPost() {
         tempPostService.deleteInvalidDateTempPost();
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 0 5 * * *", zone = "Asia/Seoul")
+    public void deleteInvalidDateAutoSavePost() {
+        autoSavePostService.deleteInvalidDateAutoSavePost();
     }
 }
