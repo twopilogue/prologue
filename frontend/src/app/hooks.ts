fbdeb1b7f764b93948bcf43b673bcfc213b3088d
taboowiths <tabou9820@gmail.com>
2024-06-18 13:01:54 +0900

    [FE] refactor: redux 제거

--- frontend/src/app/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { AppDispatch, rootState } from "./store";
-
-// Use throughout your app instead of plain `useDispatch` and `useSelector`
-export const useAppSelector: TypedUseSelectorHook<rootState> = useSelector;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
